b93b9149b7060901b9c999e0c2dc206169c357fb
JCWasmx86 <JCWasmx86@t-online.de>
2023-01-19 18:48:58 +0100

    Add type analyzer for get_option calls

---
+++ Sources/MesonAnalyze/TypeAnalyzers/OptionTypeAnalyzer.swift
@@ -0,0 +1,41 @@
+import MesonAST
+
+public class OptionTypeAnalyzer: MesonTypeAnalyzer {
+  public func derive(node: Node, fn: Function, options: [MesonOption], ns: TypeNamespace) -> [Type]
+  {
+    print("Running OptionTypeAnalyzer")
+    if let fe = node as? FunctionExpression {
+      print("Is FunctionExpression")
+      if (fe.id as! IdExpression).id != "get_option" { return fn.returnTypes }
+      if let alo = fe.argumentList {
+        print("Has ArgumentList")
+        if let al = alo as? ArgumentList {
+          print("Has ArgumentList II")
+          if al.args.count > 0 {
+            print("Has args")
+            let arg0 = al.args[0]
+            if arg0 is StringLiteral {
+              let t = (arg0 as! StringLiteral).contents()
+              let opt = options.filter({ $0.name == t }).first
+              if let o = opt {
+                if o is StringOption {
+                  return [ns.types["str"]!]
+                } else if o is IntOption {
+                  return [ns.types["int"]!]
+                } else if o is BoolOption {
+                  return [ns.types["bool"]!]
+                } else if o is FeatureOption {
+                  return [ns.types["feature"]!]
+                } else {
+                  // TODO: Can we do more?
+                  return [ListType(types: [ns.types["int"]!, ns.types["str"]!, ns.types["bool"]!])]
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+    return fn.returnTypes
+  }
+}
--- Sources/MesonAnalyze/TypeAnalyzers/TypeAnalyzersState.swift
+++ Sources/MesonAnalyze/TypeAnalyzers/TypeAnalyzersState.swift
@@ -2,7 +2,9 @@ import MesonAST
 
 public class TypeAnalyzersState {
   let state: [String: MesonTypeAnalyzer]
-  public init() { self.state = ["import": ImportTypeAnalyzer()] }
+  public init() {
+    self.state = ["import": ImportTypeAnalyzer(), "get_option": OptionTypeAnalyzer()]
+  }
   public func apply(node: Node, options: [MesonOption], f: Function, ns: TypeNamespace) -> [Type] {
     if let c = self.state[f.id()] { return c.derive(node: node, fn: f, options: options, ns: ns) }
     return f.returnTypes
