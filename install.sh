9bda7f4c92735b62b65f79215bb04a7886192e48
JCWasmx86 <JCWasmx86@t-online.de>
2023-02-05 14:21:23 +0100

    Add dnf commands to install.sh

--- install.sh
+++ install.sh
@@ -3,6 +3,15 @@ if [ -n "$(flatpak list | grep -q org.gnome.Builder.Devel)" ]; then
 	echo "Flatpak org.gnome.Builder.Devel missing"
 	exit 1
 fi
+sudo dnf install -y swift-lang \
+                    git vala meson \
+                    gcc libgee-devel \
+                    json-glib-devel gtk4-devel \
+                    gtksourceview5-devel \
+                    libadwaita-devel \
+                    libpeas-devel \
+                    template-glib-devel \
+                    g++ libsoup3-devel
 swift build -c release --static-swift-stdlib
 git clone https://github.com/JCWasmx86/GNOME-Builder-Plugins
 cd GNOME-Builder-Plugins || exit 1
