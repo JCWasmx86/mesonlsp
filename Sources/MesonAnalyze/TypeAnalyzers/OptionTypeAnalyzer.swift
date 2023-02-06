a5492de34fbee6ed9a995885fcc33227d12a2f43
JCWasmx86 <JCWasmx86@t-online.de>
2023-02-06 06:58:29 +0100

    Merge if-Statements

--- Sources/MesonAnalyze/TypeAnalyzers/OptionTypeAnalyzer.swift
+++ Sources/MesonAnalyze/TypeAnalyzers/OptionTypeAnalyzer.swift
@@ -5,30 +5,26 @@ public class OptionTypeAnalyzer: MesonTypeAnalyzer {
   {
     if let fe = node as? FunctionExpression {
       if (fe.id as! IdExpression).id != "get_option" { return fn.returnTypes }
-      if let alo = fe.argumentList {
-        if let al = alo as? ArgumentList {
-          if al.args.count > 0 {
-            let arg0 = al.args[0]
-            if arg0 is StringLiteral {
-              let t = (arg0 as! StringLiteral).contents()
-              let opt = options.filter({ $0.name == t }).first
-              if let o = opt {
-                if o is StringOption {
-                  return [ns.types["str"]!]
-                } else if o is IntOption {
-                  return [ns.types["int"]!]
-                } else if o is BoolOption {
-                  return [ns.types["bool"]!]
-                } else if o is FeatureOption {
-                  return [ns.types["feature"]!]
-                } else if o is ComboOption {
-                  // TODO: Is this right?
-                  return [ns.types["str"]!]
-                } else {
-                  // TODO: Can we do more?
-                  return [ListType(types: [ns.types["int"]!, ns.types["str"]!, ns.types["bool"]!])]
-                }
-              }
+      if let alo = fe.argumentList, let al = alo as? ArgumentList, al.args.count > 0 {
+        let arg0 = al.args[0]
+        if arg0 is StringLiteral {
+          let t = (arg0 as! StringLiteral).contents()
+          let opt = options.filter({ $0.name == t }).first
+          if let o = opt {
+            if o is StringOption {
+              return [ns.types["str"]!]
+            } else if o is IntOption {
+              return [ns.types["int"]!]
+            } else if o is BoolOption {
+              return [ns.types["bool"]!]
+            } else if o is FeatureOption {
+              return [ns.types["feature"]!]
+            } else if o is ComboOption {
+              // TODO: Is this right?
+              return [ns.types["str"]!]
+            } else {
+              // TODO: Can we do more?
+              return [ListType(types: [ns.types["int"]!, ns.types["str"]!, ns.types["bool"]!])]
             }
           }
         }
