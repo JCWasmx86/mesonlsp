a43d11f409030d70cb3352994fcfb8d037d88f25
JCWasmx86 <JCWasmx86@t-online.de>
2023-02-06 07:25:51 +0100

    Merge if-Statements

--- Sources/MesonAnalyze/Checkers/BuildAlwaysKwargCheck.swift
+++ Sources/MesonAnalyze/Checkers/BuildAlwaysKwargCheck.swift
@@ -3,20 +3,17 @@ import MesonAST
 public class BuildAlwaysKwargCheck: MesonChecker {
   public func check(node: Node, metadata: MesonMetadata) {
     if let args = (node as! FunctionExpression).argumentList as? ArgumentList {
-      for a in args.args {
-        if let kwi = a as? KeywordItem {
-          if (kwi.key as! IdExpression).id == "build_always" {
-            metadata.registerDiagnostic(
-              node: node,
-              diag: MesonDiagnostic(
-                sev: .warning,
-                node: node,
-                message: "Deprecated. Use `build_always_stale` and `build_by_default` instead"
-              )
-            )
-            break
-          }
-        }
+      for a in args.args
+      where a is KeywordItem && ((a as! KeywordItem).key as! IdExpression).id == "build_always" {
+        metadata.registerDiagnostic(
+          node: node,
+          diag: MesonDiagnostic(
+            sev: .warning,
+            node: node,
+            message: "Deprecated. Use `build_always_stale` and `build_by_default` instead"
+          )
+        )
+        break
       }
     }
   }
--- Sources/MesonAnalyze/Checkers/GuiAppKwargCheck.swift
+++ Sources/MesonAnalyze/Checkers/GuiAppKwargCheck.swift
@@ -3,20 +3,17 @@ import MesonAST
 public class GuiAppKwargCheck: MesonChecker {
   public func check(node: Node, metadata: MesonMetadata) {
     if let args = (node as! FunctionExpression).argumentList as? ArgumentList {
-      for a in args.args {
-        if let kwi = a as? KeywordItem {
-          if (kwi.key as! IdExpression).id == "gui_app" {
-            metadata.registerDiagnostic(
-              node: node,
-              diag: MesonDiagnostic(
-                sev: .warning,
-                node: node,
-                message: "Deprecated. Use `win_subsystem` instead"
-              )
-            )
-            break
-          }
-        }
+      for a in args.args
+      where a is KeywordItem && ((a as! KeywordItem).key as! IdExpression).id == "gui_app" {
+        metadata.registerDiagnostic(
+          node: node,
+          diag: MesonDiagnostic(
+            sev: .warning,
+            node: node,
+            message: "Deprecated. Use `win_subsystem` instead"
+          )
+        )
+        break
       }
     }
   }
