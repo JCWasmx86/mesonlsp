43c6a5252ad9fd2fddc35ec6fb144c8c19cbc423
JCWasmx86 <JCWasmx86@t-online.de>
2023-02-10 22:36:39 +0100

    Add shellscript to use the meson test cases. Currently about 157 failures.
    Some are expected, some not.

---
+++ do_meson_tests.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+swift build || exit
+export LSPPATH=$PWD/.build/debug/Swift-MesonLSP
+export OUTPUTPATH=$PWD/failures.txt
+rm -rf meson "$OUTPUTPATH"
+git clone https://github.com/mesonbuild/meson.git || exit
+cd "meson/test cases" || exit
+git checkout 7186279ffaf8b5827d5ba4eedbe9249bc48f82c7
+for i in *; do
+	echo "Entering testdir \"$i\""
+	cd "$i" || exit
+	for j in *; do
+		echo "Testing \"$i//$j\""
+		cd "$j" || exit
+		output=$($LSPPATH meson.build)
+		if echo "$output" | grep -q "🔴"; then
+
+			echo "$i/$j" >>"$OUTPUTPATH"
+			echo "$output" >>"$OUTPUTPATH"
+		fi
+		cd .. || exit
+	done
+	cd .. || exit
+done
+cd ../..
+rm -rf meson
+# 374
