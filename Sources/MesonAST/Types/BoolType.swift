d8d0a3aa918d64cd35c97d0c26f0a7c41e014516
JCWasmx86 <JCWasmx86@t-online.de>
2023-02-11 17:26:31 +0100

    More type fixes.
    Fixes at least csharp/3 resources

--- Sources/MesonAST/Types/BoolType.swift
+++ Sources/MesonAST/Types/BoolType.swift
@@ -5,7 +5,15 @@ public struct `BoolType`: Type {
   public init() {
     self.methods = [
       Method(name: "to_int", parent: self, returnTypes: [LazyType(name: "int")]),
-      Method(name: "to_string", parent: self, returnTypes: [LazyType(name: "str")]),
+      Method(
+        name: "to_string",
+        parent: self,
+        returnTypes: [LazyType(name: "str")],
+        args: [
+          PositionalArgument(name: "true_str", opt: true, types: [LazyType(name: "str")]),
+          PositionalArgument(name: "false_str", opt: true, types: [LazyType(name: "str")]),
+        ]
+      ),
     ]
   }
   public func toString() -> String { return "bool" }
