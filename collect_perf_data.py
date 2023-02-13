1e3da4f4c05cfdf2a6ef5863aa70a097b6c1eb21
JCWasmx86 <JCWasmx86@t-online.de>
2023-02-13 22:10:05 +0100

    Format using black

--- collect_perf_data.py
+++ collect_perf_data.py
@@ -14,32 +14,50 @@ PROJECTS = {
     "mesa": "https://gitlab.freedesktop.org/mesa/mesa/",
     "gnome-builder": "https://github.com/GNOME/gnome-builder",
     "qemu": "https://github.com/qemu/qemu",
-    "GNOME-Builder-Plugins": "https://github.com/JCWasmx86/GNOME-Builder-Plugins"
+    "GNOME-Builder-Plugins": "https://github.com/JCWasmx86/GNOME-Builder-Plugins",
 }
 N_ITERATIONS = 10
 
+
 def heaptrack(absp, d, ci):
     if not ci:
-        with subprocess.Popen(["heaptrack", "--record-only", absp, "--path", d + "/meson.build"], stdout=subprocess.PIPE, stderr=subprocess.PIPE) as prof_proces:
+        with subprocess.Popen(
+            ["heaptrack", "--record-only", absp, "--path", d + "/meson.build"],
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE,
+        ) as prof_proces:
             stdout, stderr = prof_proces.communicate()
             lines = stdout.decode("utf-8").splitlines()
-            zstfile = lines[-1].strip().split(" ")[2].replace("\"", "")
-            with subprocess.Popen(["heaptrack_print", zstfile], stdout=subprocess.PIPE, stderr=subprocess.PIPE) as ana_process:
+            zstfile = lines[-1].strip().split(" ")[2].replace('"', "")
+            with subprocess.Popen(
+                ["heaptrack_print", zstfile],
+                stdout=subprocess.PIPE,
+                stderr=subprocess.PIPE,
+            ) as ana_process:
                 stdout, stderr = ana_process.communicate()
                 lines = stdout.decode("utf-8").splitlines()
                 return lines
     else:
         # Because Ubuntu has too old software, so --record-only is not known
         # and github has no runners for modern distributions
-        with subprocess.Popen(["heaptrack", absp, "--path", d + "/meson.build"], stdout=subprocess.PIPE, stderr=subprocess.PIPE) as prof_proces:
+        with subprocess.Popen(
+            ["heaptrack", absp, "--path", d + "/meson.build"],
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE,
+        ) as prof_proces:
             stdout, stderr = prof_proces.communicate()
             lines = stdout.decode("utf-8").splitlines()
-            zstfile = lines[-1].strip().split(" ")[2].replace("\"", "")
-            with subprocess.Popen(["heaptrack_print", zstfile], stdout=subprocess.PIPE, stderr=subprocess.PIPE) as ana_process:
+            zstfile = lines[-1].strip().split(" ")[2].replace('"', "")
+            with subprocess.Popen(
+                ["heaptrack_print", zstfile],
+                stdout=subprocess.PIPE,
+                stderr=subprocess.PIPE,
+            ) as ana_process:
                 stdout, stderr = ana_process.communicate()
                 lines = stdout.decode("utf-8").splitlines()
                 return lines
-    assert(False)
+    assert False
+
 
 def analyze_file(file, commit, ci):
     ret = {}
@@ -56,12 +74,20 @@ def analyze_file(file, commit, ci):
         for d in PROJECTS:
             projobj = {}
             projobj["name"] = d
-            subprocess.run(["git", "clone", "--depth=1", PROJECTS[d]], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+            subprocess.run(
+                ["git", "clone", "--depth=1", PROJECTS[d]],
+                stdout=subprocess.DEVNULL,
+                stderr=subprocess.DEVNULL,
+            )
             a = datetime.datetime.now()
             for i in range(0, N_ITERATIONS):
-                subprocess.run([absp, "--path", d + "/meson.build"], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+                subprocess.run(
+                    [absp, "--path", d + "/meson.build"],
+                    stdout=subprocess.DEVNULL,
+                    stderr=subprocess.DEVNULL,
+                )
             b = datetime.datetime.now()
-            projobj["parsing"] = ((b - a).total_seconds() * 1000)
+            projobj["parsing"] = (b - a).total_seconds() * 1000
             lines = heaptrack(absp, d, ci)
             projobj["memory_allocations"] = int(lines[-5].split(" ")[4])
             projobj["temporary_memory_allocations"] = int(lines[-4].split(" ")[3])
@@ -70,11 +96,15 @@ def analyze_file(file, commit, ci):
             ret["projects"].append(projobj)
     print(json.dumps(ret))
 
+
 def main():
-    parser = argparse.ArgumentParser(prog="collect_perf_data.py", description="Collect performance/memory usage characteristics")
-    parser.add_argument('--ci', action='store_true')
-    parser.add_argument('filename')
-    parser.add_argument('commit')
+    parser = argparse.ArgumentParser(
+        prog="collect_perf_data.py",
+        description="Collect performance/memory usage characteristics",
+    )
+    parser.add_argument("--ci", action="store_true")
+    parser.add_argument("filename")
+    parser.add_argument("commit")
     args = parser.parse_args()
     analyze_file(args.filename, args.commit, args.ci)
 
