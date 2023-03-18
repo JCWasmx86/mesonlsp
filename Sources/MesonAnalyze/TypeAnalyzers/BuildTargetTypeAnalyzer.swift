482993720cda962d27ee90bf2d96eb416b066269
JCWasmx86 <JCWasmx86@t-online.de>
2023-03-18 10:31:08 +0100

    Refactor switch to dict access

--- Sources/MesonAnalyze/TypeAnalyzers/BuildTargetTypeAnalyzer.swift
+++ Sources/MesonAnalyze/TypeAnalyzers/BuildTargetTypeAnalyzer.swift
@@ -1,5 +1,11 @@
 import MesonAST
+
 public class BuildTargetTypeAnalyzer: MesonTypeAnalyzer {
+  static let mappings: [String: String] = [
+    "executable": "exe", "shared_library": "lib",
+    "shared_module": "build_tgt", "static_library": "lib", "both_libraries": "both_libs",
+    "library": "lib", "jar": "jar",
+  ]
   public func derive(node: Node, fn: Function, options: [MesonOption], ns: TypeNamespace) -> [Type]
   {
     if let fe = node as? FunctionExpression, let alo = fe.argumentList,
@@ -7,16 +13,11 @@ public class BuildTargetTypeAnalyzer: MesonTypeAnalyzer {
     {
       for arg in al.args where arg is KeywordItem {
         if let sl = (arg as! KeywordItem).value as? StringLiteral {
-          switch sl.contents() {
-          case "executable": return [ns.types["exe"]!]
-          case "shared_library": return [ns.types["lib"]!]
-          case "shared_module": return [ns.types["build_tgt"]!]
-          case "static_library": return [ns.types["lib"]!]
-          case "both_libraries": return [ns.types["both_libs"]!]
-          case "library": return [ns.types["lib"]!]
-          case "jar": return [ns.types["jar"]!]
-          default: break
+          let s = sl.contents()
+          if let t = BuildTargetTypeAnalyzer.mappings[s] {
+          	return [ns.types[t]!]
           }
+          break
         }
       }
     }
