c3dfe5bfc6915d3fa48da6b40997a7d402d9887b
JCWasmx86 <JCWasmx86@t-online.de>
2023-03-21 18:53:00 +0100

    Implement SVN Wrap.
    These are untested, as it's quite difficult to find SVN projects
    nowadays.
    
    Maybe I just should use random non-meson projects

--- Sources/Wrap/SvnWrap.swift
+++ Sources/Wrap/SvnWrap.swift
@@ -1,3 +1,18 @@
-public class SvnWrap: VcsWrap {
+import Foundation
 
+public class SvnWrap: VcsWrap {
+  public override func setupDirectory(path: String, packagefilesPath: String) throws {
+    try self.assertRequired("svn")
+    guard let urlAsString = self.url else { throw WrapError.genericError("Expected URL to clone") }
+    if let url = URL(string: urlAsString) {
+      guard let rev = self.revision else { throw WrapError.genericError("Missing revision") }
+      // Do something like https://github.com/mesonbuild/meson/blob/3e7c08f358e9bd91808c8ff3b76c11aedeb82f85/mesonbuild/wrap/wrap.py#L549
+      let targetDirectory =
+        self.directory ?? url.lastPathComponent.replacingOccurrences(of: ".git", with: "")
+      let fullPath = path + "/" + targetDirectory
+      try self.executeCommand(["svn", "checkout", "-r", rev, url.description, fullPath])
+    } else {
+      throw WrapError.genericError("Malformed URL: \(String(describing: self.url))")
+    }
+  }
 }
