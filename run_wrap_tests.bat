47fb8c6c16a95d3bdb5be887b84a4f135bab5612
JCWasmx86 <JCWasmx86@t-online.de>
2023-04-02 18:15:53 +0200

    Inline batch file into CI

--- run_wrap_tests.bat
+++
@@ -1,7 +0,0 @@
-mkdir __wrap_target
-swift build --static-swift-stdlib
-.\.build\debug\Swift-MesonLSP.exe --wrap Wraps/rustc-demangle.wrap^
- --wrap Wraps/libswiftdemangle.wrap --wrap Wraps/libswiftdemangle2.wrap^
- --wrap Wraps/miniz.wrap --wrap Wraps/turtle.wrap --wrap Wraps/sqlite.wrap^
- --wrap Wraps/pango.wrap --wrap Wraps/turtle2.wrap --wrap Wraps/turtle3.wrap^
- --wrap-output .\__wrap_target --wrap-package-files .\Wraps\packagefiles || exit
