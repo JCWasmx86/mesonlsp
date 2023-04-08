5aa801e8343e35ec93e55d04f1de758596476910
JCWasmx86 <JCWasmx86@t-online.de>
2023-04-08 20:12:34 +0200

    Add docs for all builtin types

--- Sources/MesonDocs/MesonDocs.swift
+++ Sources/MesonDocs/MesonDocs.swift
@@ -20,6 +20,38 @@ public class MesonDocs {
       "An array of elements. An array can contain an arbitrary number of objects of any type. Note appending to an array will always create a new array object instead of modifying the original since all objects in Meson are immutable.",
     "str": "Strings are immutable, all operations return their results as a new string.",
     "void": "Indicates that the function does not return anything. Similar to `void` in C and C++",
+    "alias_tgt": "Opaque object returned by `alias_target()`.",
+    "both_libs": "Container for both a static and shared library.",
+    "build_tgt":
+      "A build target is either an executable, shared library, static library, both shared and static library or shared module.",
+    "cfg_data":
+      "This object encapsulates configuration values to be used for generating configuration files.",
+    "compiler":
+      "This object is returned by `meson.get_compiler()`. It represents a compiler for a given language and allows you to query its properties.",
+    "custom_idx": "References a specific output file of a `custom_tgt` object.",
+    "custom_tgt": "This object is returned by `custom_target()`.",
+    "dep": "Abstract representation of a dependency.",
+    "disabler":
+      "A disabler object is an object that behaves in much the same way as NaN numbers do in floating point math. That is when used in any statement (function call, logical op, etc) they will cause the statement evaluation to immediately short circuit to return a disabler object.",
+    "env":
+      "This object is returned by `environment()` and stores detailed information about how environment variables should be set during tests. It should be passed as the env keyword argument to tests and other functions.",
+    "exe": "An executable", "external_program": "Opaque object representing an external program",
+    "extracted_obj": "Opaque object representing extracted object files from build targets",
+    "feature": "Meson object representing a `feature` option.",
+    "file": "Opaque object that stores the path to an existing file",
+    "generated_list": "Opaque object representing the result of a `generator.process()` call.",
+    "generator":
+      "This object is returned by `generator()` and contains a generator that is used to transform files from one type to another by an executable (e.g. idl files into source code and headers).",
+    "inc": "Opaque wrapper for storing include directories", "jar": "A Java JAR build target",
+    "lib": "Represents either a shared or static library", "module": "Base type for all modules.",
+    "range": "Opaque object that can be used in a loop and accessed via `[num]`.",
+    "run_tgt": "Opaque object returned by `run_target()`.",
+    "runresult":
+      "This object encapsulates the result of trying to compile and run a sample piece of code with `compiler.run()` or `run_command()`.",
+    "structured_src": "Opaque object returned by `structured_sources()`.",
+    "subproject":
+      "This object is returned by `subproject()` and is an opaque object representing it.",
+    "tgt": "Opaque base object for all Meson targets",
   ]
 
   public init() {
