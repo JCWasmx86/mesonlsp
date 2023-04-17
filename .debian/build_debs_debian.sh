a548a12783642696bede257e4de8aa4a80719372
JCWasmx86 <JCWasmx86@t-online.de>
2023-04-17 21:35:53 +0200

    Use gzip instead of zstd

--- .debian/build_debs_debian.sh
+++ .debian/build_debs_debian.sh
@@ -3,15 +3,15 @@ cd .debian || exit
 chmod 0755 build/DEBIAN
 mkdir -p build/usr/bin
 cp ../out/Swift-MesonLSP build/usr/bin
-dpkg-deb --build build
+dpkg-deb -Zgzip --build build
 cp build.deb /Swift-MesonLSP-debian-stable.deb || exit 1
 rm build.deb build/usr/bin/Swift-MesonLSP
 cp ../out1/Swift-MesonLSP build/usr/bin
-dpkg-deb --build build
+dpkg-deb -Zgzip --build build
 cp build.deb /Swift-MesonLSP-debian-testing.deb || exit 1
 rm build.deb build/usr/bin/Swift-MesonLSP
 cp ../out2/Swift-MesonLSP build/usr/bin
-dpkg-deb --build build
+dpkg-deb -Zgzip --build build
 cp build.deb /Swift-MesonLSP-debian-unstable.deb || exit 1
 rm build.deb build/usr/bin/Swift-MesonLSP
 exit 0
--- .debian/build_debs_ubuntu.sh
+++ .debian/build_debs_ubuntu.sh
@@ -3,15 +3,15 @@ cd .debian || exit
 chmod 0755 build/DEBIAN
 mkdir -p build/usr/bin
 cp ../out/Swift-MesonLSP build/usr/bin
-dpkg-deb --build build
+dpkg-deb -Zgzip --build build
 cp build.deb /Swift-MesonLSP-ubuntu-18.04.deb || exit 1
 rm build.deb build/usr/bin/Swift-MesonLSP
 cp ../out1/Swift-MesonLSP build/usr/bin
-dpkg-deb --build build
+dpkg-deb -Zgzip --build build
 cp build.deb /Swift-MesonLSP-ubuntu-20.04.deb || exit 1
 rm build.deb build/usr/bin/Swift-MesonLSP
 cp ../out2/Swift-MesonLSP build/usr/bin
-dpkg-deb --build build
+dpkg-deb -Zgzip --build build
 cp build.deb /Swift-MesonLSP-ubuntu-22.04.deb || exit 1
 rm build.deb build/usr/bin/Swift-MesonLSP
 exit 0
