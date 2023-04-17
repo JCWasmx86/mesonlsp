be6a07922540ddd35e25340389ed4cc396b8be6c
JCWasmx86 <JCWasmx86@t-online.de>
2023-04-17 20:33:01 +0200

    Misc fixes

--- .debian/build_debs_ubuntu.sh
+++ .debian/build_debs_ubuntu.sh
@@ -5,12 +5,12 @@ mkdir -p build/usr/bin
 cp ../out/Swift-MesonLSP build/usr/bin
 dpkg-deb --build build
 cp build.deb /Swift-MesonLSP-ubuntu-18.04.deb
-rm build.deb usr/bin/Swift-MesonLSP
+rm build.deb build/usr/bin/Swift-MesonLSP
 cp ../out1/Swift-MesonLSP build/usr/bin
-dpkg-deb --build .
+dpkg-deb --build build
 cp build.deb /Swift-MesonLSP-ubuntu-20.04.deb
-rm .deb usr/bin/Swift-MesonLSP
+rm build.deb build/usr/bin/Swift-MesonLSP
 cp ../out2/Swift-MesonLSP build/usr/bin
-dpkg-deb --build .
+dpkg-deb --build build
 cp build.deb /Swift-MesonLSP-ubuntu-22.04.deb
 rm build.deb usr/bin/Swift-MesonLSP
