8d2a1c8e5557bf161e20276810b2334d110d0d00
JCWasmx86 <JCWasmx86@t-online.de>
2023-04-17 21:04:15 +0200

    Fix

--- .debian/build_debs_debian.sh
+++ .debian/build_debs_debian.sh
@@ -4,13 +4,14 @@ chmod 0755 build/DEBIAN
 mkdir -p build/usr/bin
 cp ../out/Swift-MesonLSP build/usr/bin
 dpkg-deb --build build
-cp build.deb /Swift-MesonLSP-debian-stable.deb
+cp build.deb /Swift-MesonLSP-debian-stable.deb || exit 1
 rm build.deb build/usr/bin/Swift-MesonLSP
 cp ../out1/Swift-MesonLSP build/usr/bin
 dpkg-deb --build build
-cp build.deb /Swift-MesonLSP-debian-testing.deb
+cp build.deb /Swift-MesonLSP-debian-testing.deb || exit 1
 rm build.deb build/usr/bin/Swift-MesonLSP
 cp ../out2/Swift-MesonLSP build/usr/bin
 dpkg-deb --build build
-cp build.deb /Swift-MesonLSP-debian-unstable.deb
+cp build.deb /Swift-MesonLSP-debian-unstable.deb || exit 1
 rm build.deb build/usr/bin/Swift-MesonLSP
+exit 0
--- .debian/build_debs_ubuntu.sh
+++ .debian/build_debs_ubuntu.sh
@@ -4,13 +4,14 @@ chmod 0755 build/DEBIAN
 mkdir -p build/usr/bin
 cp ../out/Swift-MesonLSP build/usr/bin
 dpkg-deb --build build
-cp build.deb /Swift-MesonLSP-ubuntu-18.04.deb
+cp build.deb /Swift-MesonLSP-ubuntu-18.04.deb || exit 1
 rm build.deb build/usr/bin/Swift-MesonLSP
 cp ../out1/Swift-MesonLSP build/usr/bin
 dpkg-deb --build build
-cp build.deb /Swift-MesonLSP-ubuntu-20.04.deb
+cp build.deb /Swift-MesonLSP-ubuntu-20.04.deb || exit 1
 rm build.deb build/usr/bin/Swift-MesonLSP
 cp ../out2/Swift-MesonLSP build/usr/bin
 dpkg-deb --build build
-cp build.deb /Swift-MesonLSP-ubuntu-22.04.deb
-rm build.deb usr/bin/Swift-MesonLSP
+cp build.deb /Swift-MesonLSP-ubuntu-22.04.deb || exit 1
+rm build.deb build/usr/bin/Swift-MesonLSP
+exit 0
