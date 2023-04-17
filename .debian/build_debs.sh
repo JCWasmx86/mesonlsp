0f767091be7a8692f0ac192ca204c808a2ac8695
JCWasmx86 <JCWasmx86@t-online.de>
2023-04-17 15:45:14 +0200

    Move DEBIAN to subdir

--- .debian/build_debs.sh
+++ .debian/build_debs.sh
@@ -1,16 +1,16 @@
 #!/usr/bin/env bash
 cd .debian || exit
 chmod 0755 DEBIAN
-mkdir -p usr/bin
-cp ../out/Swift-MesonLSP usr/bin
+mkdir -p build/usr/bin
+cp ../out/Swift-MesonLSP build/usr/bin
+dpkg-deb --build build
+cp build.deb /Swift-MesonLSP-ubuntu-18.04.deb
+rm build.deb usr/bin/Swift-MesonLSP
+cp ../out1/Swift-MesonLSP build/usr/bin
 dpkg-deb --build .
-cp ..deb /Swift-MesonLSP-ubuntu-18.04.deb
-rm ..deb usr/bin/Swift-MesonLSP
-cp ../out1/Swift-MesonLSP usr/bin
+cp build.deb /Swift-MesonLSP-ubuntu-20.04.deb
+rm .deb usr/bin/Swift-MesonLSP
+cp ../out2/Swift-MesonLSP build/usr/bin
 dpkg-deb --build .
-cp ..deb /Swift-MesonLSP-ubuntu-20.04.deb
-rm ..deb usr/bin/Swift-MesonLSP
-cp ../out2/Swift-MesonLSP usr/bin
-dpkg-deb --build .
-cp ..deb /Swift-MesonLSP-ubuntu-22.04.deb
-rm ..deb usr/bin/Swift-MesonLSP
+cp build.deb /Swift-MesonLSP-ubuntu-22.04.deb
+rm build.deb usr/bin/Swift-MesonLSP
