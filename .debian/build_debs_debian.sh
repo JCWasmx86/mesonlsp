9765ad41c663581d611d8aa6498c353b14679279
JCWasmx86 <JCWasmx86@t-online.de>
2023-04-18 05:47:04 +0200

    Include licenses in .deb files

--- .debian/build_debs_debian.sh
+++ .debian/build_debs_debian.sh
@@ -1,7 +1,11 @@
 #!/usr/bin/env bash
+./create_license_bundle.sh
 cd .debian || exit
 chmod 0755 build/DEBIAN
 mkdir -p build/usr/bin
+mkdir -p build/usr/share/Swift-MesonLSP
+cp ../COPYING build/usr/share/Swift-MesonLSP/
+cp ../3rdparty.txt build/usr/share/Swift-MesonLSP/
 cp ../out/Swift-MesonLSP build/usr/bin
 dpkg-deb -Zgzip --build build
 cp build.deb /Swift-MesonLSP-debian-stable.deb || exit 1
--- .debian/build_debs_ubuntu.sh
+++ .debian/build_debs_ubuntu.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/env bash
+./create_license_bundle.sh
 cd .debian || exit
 chmod 0755 build/DEBIAN
+mkdir -p build/usr/share/Swift-MesonLSP
+cp ../COPYING build/usr/share/Swift-MesonLSP/
+cp ../3rdparty.txt build/usr/share/Swift-MesonLSP/
 mkdir -p build/usr/bin
 cp ../out/Swift-MesonLSP build/usr/bin
 dpkg-deb -Zgzip --build build
