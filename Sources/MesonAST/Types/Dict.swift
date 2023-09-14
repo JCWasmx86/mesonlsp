e6fd98fec4bc517ea492784850f43f27534e8d87
JCWasmx86 <JCWasmx86@t-online.de>
2023-09-14 15:28:09 +0200

    Cache toString() for dicts and lists

--- Sources/MesonAST/Types/Dict.swift
+++ Sources/MesonAST/Types/Dict.swift
@@ -1,9 +1,11 @@
 public final class Dict: Type {
   public let name: String = "dict"
   public let types: [Type]
+  private let cache: String
 
-  public init(types: [Type]) { self.types = types }
-  public func toString() -> String {
-    return "dict(" + self.types.map { $0.toString() }.sorted().joined(separator: "|") + ")"
+  public init(types: [Type]) {
+    self.types = types
+    self.cache = "dict(" + self.types.map { $0.toString() }.sorted().joined(separator: "|") + ")"
   }
+  public func toString() -> String { return self.cache }
 }
--- Sources/MesonAST/Types/List.swift
+++ Sources/MesonAST/Types/List.swift
@@ -1,10 +1,12 @@
 public final class ListType: Type {
   public let name: String = "list"
   public let types: [Type]
+  private let cache: String
 
-  public init(types: [Type]) { self.types = types }
-
-  public func toString() -> String {
-    return "list(" + self.types.map { $0.toString() }.sorted().joined(separator: "|") + ")"
+  public init(types: [Type]) {
+    self.types = types
+    self.cache = "list(" + self.types.map { $0.toString() }.sorted().joined(separator: "|") + ")"
   }
+
+  public func toString() -> String { return self.cache }
 }
