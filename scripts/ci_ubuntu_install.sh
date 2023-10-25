63ab13a30df02ee806e402308687bbe30fd00fa5
JCWasmx86 <JCWasmx86@t-online.de>
2023-10-25 15:32:39 +0200

    Fix

--- scripts/ci_ubuntu_install.sh
+++ scripts/ci_ubuntu_install.sh
@@ -6,7 +6,7 @@ echo "Detected Ubuntu version: $ubuntu_version"
 case "$ubuntu_version" in
 "18.04")
         apt update
-	apt install -y install binutils gnupg2 libc6-dev libcurl4-openssl-dev libedit2 libgcc-8-dev libpython3.8 libsqlite3-0 libstdc++-8-dev libxml2-dev libz3-dev pkg-config tzdata unzip zlib1g-dev -y
+	apt install -y binutils gnupg2 libc6-dev libcurl4-openssl-dev libedit2 libgcc-8-dev libpython3.8 libsqlite3-0 libstdc++-8-dev libxml2-dev libz3-dev pkg-config tzdata unzip zlib1g-dev -y
 	;;
 "20.04")
         apt update
