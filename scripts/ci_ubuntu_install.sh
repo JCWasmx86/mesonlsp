75bebc53fe0ac5d736dfc8c83b8e2284a49b6c46
JCWasmx86 <JCWasmx86@t-online.de>
2023-10-25 17:00:00 +0200

    Format shell scripts

--- scripts/ci_ubuntu_install.sh
+++ scripts/ci_ubuntu_install.sh
@@ -7,15 +7,15 @@ export DEBIAN_FRONTEND=noninteractive
 export TZ=Etc/UTC
 case "$ubuntu_version" in
 "18.04")
-        apt update
+	apt update
 	apt install -y binutils gnupg2 libc6-dev libcurl4-openssl-dev libedit2 libgcc-8-dev libpython3.8 libsqlite3-0 libstdc++-8-dev libxml2-dev libz3-dev pkg-config tzdata unzip zlib1g-dev -y
 	;;
 "20.04")
-        apt update
+	apt update
 	apt install -y binutils gnupg2 libc6-dev libcurl4-openssl-dev libedit2 libgcc-10-dev libpython3.8 libsqlite3-0 libstdc++-10-dev libxml2-dev libz3-dev pkg-config tzdata unzip zlib1g-dev
 	;;
 "22.04")
-        apt update
+	apt update
 	apt install -y binutils gnupg2 libc6-dev libcurl4-openssl-dev libedit2 libgcc-10-dev libpython3.10 libsqlite3-0 libstdc++-10-dev libxml2-dev libz3-dev pkg-config tzdata unzip zlib1g-dev
 	;;
 *)
