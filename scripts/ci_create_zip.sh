379a908a02ca9236842a6ab58d1c5b2ff523a287
JCWasmx86 <JCWasmx86@t-online.de>
2023-10-25 14:51:23 +0200

    More attempts to fix stuff

--- scripts/ci_create_zip.sh
+++ scripts/ci_create_zip.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 swift build --static-swift-stdlib || exit 1
 cp .build/debug/Swift-MesonLSP Swift-MesonLSP.debug
-swift test --static-swift-stdlib || exit 1
+rm -rf .build/
+swift test || exit 1
+rm -rf .build/
 swift build -c release --static-swift-stdlib || exit 1
 cp .build/release/Swift-MesonLSP Swift-MesonLSP
 zip -9 "$1".zip Swift-MesonLSP.debug Swift-MesonLSP
---
+++ scripts/ci_ubuntu_install.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+ubuntu_version=$(lsb_release -rs)
+echo "Detected Ubuntu version: $ubuntu_version"
+case "$ubuntu_version" in
+"18.04")
+        sudo apt update
+	sudo apt install -y install binutils gnupg2 libc6-dev libcurl4-openssl-dev libedit2 libgcc-8-dev libpython3.8 libsqlite3-0 libstdc++-8-dev libxml2-dev libz3-dev pkg-config tzdata unzip zlib1g-dev -y
+	;;
+"20.04")
+        sudo apt update
+	sudo apt install -y binutils gnupg2 libc6-dev libcurl4-openssl-dev libedit2 libgcc-10-dev libpython3.8 libsqlite3-0 libstdc++-10-dev libxml2-dev libz3-dev pkg-config tzdata unzip zlib1g-dev
+	;;
+"22.04")
+        sudo apt update
+	sudo apt install -y apt-get install binutils gnupg2 libc6-dev libcurl4-openssl-dev libedit2 libgcc-10-dev libpython3.8 libsqlite3-0 libstdc++-10-dev libxml2-dev libz3-dev pkg-config tzdata unzip zlib1g-dev
+	;;
+*)
+	echo "Unsupported Ubuntu version: $ubuntu_version. This script supports 18.04, 20.04, and 22.04."
+	exit 1
+	;;
+esac
