a342775d9c7e15a9cdaa1dedce2cc99a014e1a7b
JCWasmx86 <JCWasmx86@t-online.de>
2023-10-25 15:31:17 +0200

    Use Python 3.10

--- scripts/ci_ubuntu_install.sh
+++ scripts/ci_ubuntu_install.sh
@@ -14,7 +14,7 @@ case "$ubuntu_version" in
 	;;
 "22.04")
         apt update
-	apt install -y apt-get install binutils gnupg2 libc6-dev libcurl4-openssl-dev libedit2 libgcc-10-dev libpython3.8 libsqlite3-0 libstdc++-10-dev libxml2-dev libz3-dev pkg-config tzdata unzip zlib1g-dev
+	apt install -y binutils gnupg2 libc6-dev libcurl4-openssl-dev libedit2 libgcc-10-dev libpython3.10 libsqlite3-0 libstdc++-10-dev libxml2-dev libz3-dev pkg-config tzdata unzip zlib1g-dev
 	;;
 *)
 	echo "Unsupported Ubuntu version: $ubuntu_version. This script supports 18.04, 20.04, and 22.04."
