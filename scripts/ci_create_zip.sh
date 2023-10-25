bf618ddb45c2dfa6d5d341305e5c078c348315db
JCWasmx86 <JCWasmx86@t-online.de>
2023-10-25 14:40:28 +0200

    Use static stdlib everywhere

--- scripts/ci_create_zip.sh
+++ scripts/ci_create_zip.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 swift build --static-swift-stdlib || exit 1
 cp .build/debug/Swift-MesonLSP Swift-MesonLSP.debug
-swift test || exit 1
+swift test --static-swift-stdlib || exit 1
 swift build -c release --static-swift-stdlib || exit 1
 cp .build/release/Swift-MesonLSP Swift-MesonLSP
 zip -9 "$1".zip Swift-MesonLSP.debug Swift-MesonLSP
