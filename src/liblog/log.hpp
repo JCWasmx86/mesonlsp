0d3f42a7d9eb2b0c898460542812ae7e169da604
JCWasmx86 <JCWasmx86@t-online.de>
2023-12-03 12:40:11 +0100

    Add log library

---
+++ src/liblog/log.hpp
@@ -0,0 +1,20 @@
+#pragma once
+
+#include <iostream>
+#include <source_location>
+#include <string>
+
+class Logger
+{
+private:
+  std::string module;
+
+public:
+  Logger (std::string module);
+  void
+  info (std::string msg, const std::source_location location = std::source_location::current ());
+  void
+  error (std::string msg, const std::source_location location = std::source_location::current ());
+  void
+  warn (std::string msg, const std::source_location location = std::source_location::current ());
+};
---
+++ tests/liblog/test.cpp
@@ -0,0 +1,10 @@
+#include <log.cpp>
+
+int
+main (int argc, char **argv)
+{
+  Logger log ("my-module");
+  log.error ("I'm an error");
+  log.info ("I'm info");
+  log.warn ("I'm a warning");
+}
