1e5e1765775642b3265796ffdb11db389a0febe4
JCWasmx86 <JCWasmx86@t-online.de>
2023-12-03 13:33:40 +0100

    Add some basic stuff

--- src/liblog/log.hpp
+++ src/liblog/log.hpp
@@ -11,10 +11,10 @@ private:
 
 public:
   Logger (std::string module);
-  void
-  info (std::string msg, const std::source_location location = std::source_location::current ());
-  void
-  error (std::string msg, const std::source_location location = std::source_location::current ());
-  void
-  warn (std::string msg, const std::source_location location = std::source_location::current ());
+  void info (std::string msg, const std::source_location location
+                              = std::source_location::current ());
+  void error (std::string msg, const std::source_location location
+                               = std::source_location::current ());
+  void warn (std::string msg, const std::source_location location
+                              = std::source_location::current ());
 };
---
+++ src/libobjects/function.cpp
@@ -0,0 +1,14 @@
+#include "function.hpp"
+#include <string>
+
+const std::string
+Function::id ()
+{
+  return this->name;
+}
+
+const std::string
+Method::id ()
+{
+  return this->name;
+}
