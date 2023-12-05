64b75d7b7be9d0da6e103e79152505d858635324
JCWasmx86 <JCWasmx86@t-online.de>
2023-12-05 06:40:44 +0100

    Add first ts->node mapping

--- src/libast/sourcefile.cpp
+++ src/libast/sourcefile.cpp
@@ -1,1 +1,17 @@
 #include "sourcefile.hpp"
+#include <fstream>
+
+std::string MesonSourceFile::contents() {
+  if (this->cached) {
+    return this->cached_contents;
+  }
+  auto path = std::filesystem::path(this->file);
+  std::ifstream file(path);
+  auto file_size = std::filesystem::file_size(path);
+  std::string file_content;
+  file_content.resize(file_size, '\0');
+  file.read(file_content.data(), file_size);
+  this->cached = true;
+  this->cached_contents = file_content;
+  return file_content;
+}
--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,29 @@
+#include "libast/sourcefile.hpp"
+#include <cstddef>
+#include <cstdlib>
+#include <filesystem>
+#include <fstream>
+#include <memory>
+#include <node.hpp>
+#include <sourcefile.hpp>
 #include <tree_sitter/api.h>
 
 extern "C" TSLanguage *tree_sitter_meson();
 int main(int argc, char **argv) {
   TSParser *parser = ts_parser_new();
   ts_parser_set_language(parser, tree_sitter_meson());
+  auto path = std::filesystem::path(argv[1]);
+  std::ifstream file(path);
+  auto file_size = std::filesystem::file_size(path);
+  std::string file_content;
+  file_content.resize(file_size, '\0');
+  file.read(file_content.data(), file_size);
+  TSTree *tree = ts_parser_parse_string(parser, NULL, file_content.data(),
+                                        file_content.length());
+  TSNode root_node = ts_tree_root_node(tree);
+  auto source_file = std::make_shared<MesonSourceFile>(path);
+  auto root = make_node(source_file, root_node);
+
+  ts_tree_delete(tree);
   ts_parser_delete(parser);
 }
