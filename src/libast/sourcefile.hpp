e83ce71b7380ab264073b9c212032e53d0ec735c
JCWasmx86 <JCWasmx86@t-online.de>
2023-12-05 19:11:26 +0100

    Implement nearly all nodes completely

--- src/libast/sourcefile.hpp
+++ src/libast/sourcefile.hpp
@@ -1,6 +1,8 @@
 #pragma once
 
 #include <filesystem>
+#include <string>
+#include <tree_sitter/api.h>
 
 class MesonSourceFile {
 public:
@@ -9,6 +11,11 @@ public:
   MesonSourceFile(std::filesystem::path file) : file(file) {}
   virtual std::string contents();
   virtual ~MesonSourceFile() = default;
+  std::string extract_node_value(TSNode node) {
+    auto start_byte = ts_node_start_byte(node);
+    auto end_byte = ts_node_end_byte(node);
+    return this->contents().substr(start_byte, end_byte - start_byte);
+  }
 
 private:
   std::string cached_contents;
