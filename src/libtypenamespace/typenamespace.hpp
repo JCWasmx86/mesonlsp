cf25b8b51d5aa827a6e1714ac5dac1143df7ad5f
JCWasmx86 <JCWasmx86@t-online.de>
2023-12-10 08:22:17 +0100

    Add scaffolding for libtypenamespace

---
+++ src/libtypenamespace/typenamespace.hpp
@@ -0,0 +1,20 @@
+#pragma once
+
+#include "function.hpp"
+#include "type.hpp"
+#include <map>
+#include <memory>
+#include <string>
+#include <vector>
+
+class TypeNamespace {
+public:
+  std::vector<std::shared_ptr<Function>> functions;
+  std::map<std::string, std::shared_ptr<Type>> types;
+  std::map<std::string, std::vector<std::shared_ptr<Method>>> vtables;
+  std::shared_ptr<Str> strType;
+  std::shared_ptr<IntType> intType;
+  std::shared_ptr<BoolType> boolType;
+
+  TypeNamespace();
+};
