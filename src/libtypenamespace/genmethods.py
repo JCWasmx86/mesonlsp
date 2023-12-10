a7a1d3fc3c9c27571f3f4f2f4ae500ca2452c4b9
JCWasmx86 <JCWasmx86@t-online.de>
2023-12-10 12:24:49 +0100

    Improve scripts

--- src/libtypenamespace/genmethods.py
+++ src/libtypenamespace/genmethods.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 import sys
-
 from collections import defaultdict
+
 
 def extract_types(input_str):
     input_str = input_str[5:][:-1]
@@ -23,25 +23,27 @@ def extract_types(input_str):
         ret.append(s)
     return ret
 
+
 def type_to_cpp(t: str):
     if t == "subproject()":
-        return "this->types[\"subproject\"]"
+        return 'this->types["subproject"]'
     if t.startswith("dict(") or t.startswith("list("):
         cpp_type = "Dict" if t.startswith("dict(") else "List"
         sub_types = map(type_to_cpp, extract_types(t))
         total_str = "{" + ",".join(sub_types) + "}"
         return f"std::make_shared<{cpp_type}>(std::vector<std::shared_ptr<Type>>{total_str})"
-    return f"this->types[\"{t}\"]"
+    return f'this->types["{t}"]'
+
 
 def main():
     with open(sys.argv[2], "w", encoding="utf-8") as output:
         with open(sys.argv[1], "r", encoding="utf-8") as filep:
             lines = filep.readlines()
-        print('#include <memory>', file=output)
+        print("#include <memory>", file=output)
         print('#include "typenamespace.hpp"', file=output)
         print('#include "type.hpp"', file=output)
-        print('#define True true', file=output)
-        print('#define False false', file=output)
+        print("#define True true", file=output)
+        print("#define False false", file=output)
         print("void TypeNamespace::initMethods() {", file=output)
         idx = 0
         curr_fn_name = None
@@ -73,22 +75,26 @@ def main():
                         varargs = "true" in lines[idx]
                         idx += 1
                         arg_types = []
-                        idx += 1 # Skip "- Types:"
+                        idx += 1  # Skip "- Types:"
                         while True:
                             if not lines[idx].startswith("        -"):
                                 break
-                            arg_types.append(lines[idx].replace("        -", "").strip())
+                            arg_types.append(
+                                lines[idx].replace("        -", "").strip()
+                            )
                             idx += 1
                         args.append((arg_name, varargs, optional, arg_types))
                     else:
                         optional = "true" in lines[idx]
                         idx += 1
                         arg_types = []
-                        idx += 1 # Skip "- Types:"
+                        idx += 1  # Skip "- Types:"
                         while True:
                             if not lines[idx].startswith("        -"):
                                 break
-                            arg_types.append(lines[idx].replace("        -", "").strip())
+                            arg_types.append(
+                                lines[idx].replace("        -", "").strip()
+                            )
                             idx += 1
                         kwargs.append((arg_name, optional, arg_types))
             elif lines[idx].startswith("  - returns:"):
@@ -96,23 +102,34 @@ def main():
                 while idx != len(lines):
                     if not lines[idx].startswith("    -"):
                         break
-                    returns.append(lines[idx].replace("    -", "").replace(":", "").strip())
+                    returns.append(
+                        lines[idx].replace("    -", "").replace(":", "").strip()
+                    )
                     idx += 1
         for obj_name in sorted(full_data.keys()):
-            print(f"  this->vtables[\"{obj_name}\"] = std::vector<std::shared_ptr<Method>>" + "{", file=output)
+            print(
+                f'  this->vtables["{obj_name}"] = std::vector<std::shared_ptr<Method>>'
+                + "{",
+                file=output,
+            )
             for idx, method in enumerate(full_data[obj_name]):
                 print("    std::make_shared<Method>(", file=output)
-                print(f"      \"{method[0]}\",", file=output)
+                print(f'      "{method[0]}",', file=output)
                 print("      std::vector<std::shared_ptr<Argument>> {", file=output)
                 args = method[1]
                 kwargs = method[2]
                 total_len = len(args) + len(kwargs)
                 for idx_, arg in enumerate(args):
                     print("        std::make_shared<PositionalArgument>(", file=output)
-                    print(f"          \"{arg[0]}\",", file=output)
-                    print("          std::vector<std::shared_ptr<Type>>{", file=output),
+                    print(f'          "{arg[0]}",', file=output)
+                    print("          std::vector<std::shared_ptr<Type>>{", file=output)
                     for t_idx, t in enumerate(arg[3]):
-                        print("            " + type_to_cpp(t) + ("," if t_idx != len(arg[3]) - 1 else ""), file=output)
+                        print(
+                            "            "
+                            + type_to_cpp(t)
+                            + ("," if t_idx != len(arg[3]) - 1 else ""),
+                            file=output,
+                        )
                     print("          },", file=output)
                     print(f"          {arg[2]},", file=output)
                     print(f"          {arg[1]}", file=output)
@@ -122,10 +139,15 @@ def main():
                         print("        ),", file=output)
                 for idx_, arg in enumerate(kwargs):
                     print("        std::make_shared<Kwarg>(", file=output)
-                    print(f"          \"{arg[0][1:]}\",", file=output)
-                    print("          std::vector<std::shared_ptr<Type>>{", file=output),
+                    print(f'          "{arg[0][1:]}",', file=output)
+                    print("          std::vector<std::shared_ptr<Type>>{", file=output)
                     for t_idx, t in enumerate(arg[2]):
-                        print("            " + type_to_cpp(t) + ("," if t_idx != len(arg[2]) - 1 else ""), file=output)
+                        print(
+                            "            "
+                            + type_to_cpp(t)
+                            + ("," if t_idx != len(arg[2]) - 1 else ""),
+                            file=output,
+                        )
                     print("          },", file=output)
                     print(f"          {arg[1]}", file=output)
                     if len(args) + idx_ == total_len - 1:
@@ -135,14 +157,21 @@ def main():
                 print("      },", file=output)
                 print("      std::vector<std::shared_ptr<Type>>{", file=output)
                 for t_idx, t in enumerate(method[3]):
-                    print("        " + type_to_cpp(t) + ("," if t_idx != len(method[3]) - 1 else ""), file=output)
+                    print(
+                        "        "
+                        + type_to_cpp(t)
+                        + ("," if t_idx != len(method[3]) - 1 else ""),
+                        file=output,
+                    )
                 print("      },", file=output)
-                print(f"     this->types[\"{obj_name}\"]", file=output)
+                print(f'     this->types["{obj_name}"]', file=output)
                 if idx == len(full_data[obj_name]) - 1:
                     print("    )", file=output)
                 else:
                     print("    ),", file=output)
             print("  };", file=output)
         print("}", file=output)
+
+
 if __name__ == "__main__":
     main()
