8b52497fed282041061bced3f0c76d88ac425575
JCWasmx86 <JCWasmx86@t-online.de>
2023-12-12 15:05:01 +0100

    Add first setup stuff for Hg/Svn/Git wraps

---
+++ src/libwrap/hgwrap.cpp
@@ -0,0 +1,31 @@
+#include "ini.hpp"
+#include "utils.hpp"
+#include "wrap.hpp"
+#include <format>
+
+void HgWrap::setupDirectory(std::filesystem::path path,
+                            std::filesystem::path packageFilesPath) {
+  auto url = this->url;
+  if (url.empty()) {
+    return;
+  }
+  if (this->revision.empty())
+    return;
+  std::string rev = this->revision;
+  if (this->directory->empty()) {
+    return;
+  }
+  auto targetDirectory = this->directory.value();
+  std::string fullPath = std::format("{}/{}", path.c_str(), targetDirectory);
+  auto result =
+      launchProcess("hg", std::vector<std::string>{"clone", url, fullPath});
+  if (result) {
+    auto is_tip = rev.size() == 3 &&
+                  (std::tolower(rev[0]) == 't' && std::tolower(rev[1]) == 'i' &&
+                   std::tolower(rev[2]) == 'p');
+    if (is_tip) {
+      result = launchProcess(
+          "hg", std::vector<std::string>{"--cwd", fullPath, "checkout", rev});
+    }
+  }
+}
