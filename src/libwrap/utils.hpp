f1d3d79294c95f3710e7a4bad1625155e4c8796d
JCWasmx86 <JCWasmx86@t-online.de>
2023-12-12 09:44:29 +0100

    Add function to launch process

--- src/libwrap/utils.hpp
+++ src/libwrap/utils.hpp
@@ -1,7 +1,11 @@
 #pragma once
 
 #include <filesystem>
+#include <string>
+#include <vector>
 
 bool download_file(std::string url, std::filesystem::path output);
 bool extract_file(std::filesystem::path archive_path,
                   std::filesystem::path output_directory);
+bool launchProcess(std::string file, std::vector<std::string> args);
+std::string errno2string();
