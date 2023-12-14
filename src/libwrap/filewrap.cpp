e06ae921dc20968d1eabce96cd2c431dadf40e42
JCWasmx86 <JCWasmx86@t-online.de>
2023-12-14 11:24:05 +0100

    Partially implement FileWrap::setupDirectory

--- src/libwrap/filewrap.cpp
+++ src/libwrap/filewrap.cpp
@@ -1,5 +1,8 @@
 #include "ini.hpp"
+#include "utils.hpp"
 #include "wrap.hpp"
+#include <filesystem>
+#include <format>
 
 FileWrap::FileWrap(ast::ini::Section *section) : Wrap(section) {
   if (auto val = section->find_string_value("source_url")) {
@@ -17,4 +20,34 @@ FileWrap::FileWrap(ast::ini::Section *section) : Wrap(section) {
   if (auto val = section->find_string_value("lead_directory_missing")) {
     this->leadDirectoryMissing = val == "true";
   }
+}
+
+void FileWrap::setupDirectory(std::filesystem::path path,
+                              std::filesystem::path packageFilesPath) {
+  auto url = this->sourceUrl;
+  if (url.empty())
+    return;
+  auto hash = this->sourceHash;
+  if (hash->empty())
+    return;
+  auto sfn = this->sourceFilename;
+  if (sfn->empty())
+    return;
+  auto directory = this->directory;
+  std::string targetDirectory;
+  if (directory.has_value() && directory.value().empty())
+    targetDirectory = directory.value();
+  else {
+    size_t lastDotPos = sfn->find_last_of('.');
+    auto result =
+        (lastDotPos != std::string::npos) ? sfn->substr(0, lastDotPos) : sfn;
+    targetDirectory = result.value();
+  }
+  auto fullPath = std::format("{}/{}", path.c_str(), targetDirectory);
+  // TODO: Caching
+  auto archiveFileName = std::filesystem::path{random_file()};
+  download_file(url, archiveFileName);
+  auto wd = this->leadDirectoryMissing ? std::filesystem::path{fullPath} : path;
+  std::filesystem::create_directories(wd);
+  extract_file(archiveFileName, wd);
 }
