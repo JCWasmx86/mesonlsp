d96d744ec303b6cc38c182201fc19be308cd1520
JCWasmx86 <JCWasmx86@t-online.de>
2023-12-16 12:24:42 +0100

    Deduplicate code

--- src/libanalyze/mesontree.cpp
+++ src/libanalyze/mesontree.cpp
@@ -10,51 +10,42 @@ extern "C" TSLanguage *tree_sitter_meson(); // NOLINT
 
 static Logger LOG("analyze::mesontree"); // NOLINT
 
-void MesonTree::fastParse(AnalysisOptions analysisOptions) {
-  OptionState optionState;
-  auto modernOptionsFile = this->root / "meson.options";
-  LOG.info(modernOptionsFile.generic_string());
-  // First parse the meson_options.txt/meson.options file
+OptionState parseFile(std::filesystem::path path) {
   auto visitor = OptionExtractor();
+  TSParser *parser = ts_parser_new();
+  ts_parser_set_language(parser, tree_sitter_meson());
+  std::ifstream file(path.c_str());
+  auto fileSize = std::filesystem::file_size(path);
+  std::string fileContent;
+  fileContent.resize(fileSize, '\0');
+  file.read(fileContent.data(), fileSize);
+  TSTree *tree = ts_parser_parse_string(parser, nullptr, fileContent.data(),
+                                        fileContent.length());
+  TSNode rootNode = ts_tree_root_node(tree);
+  auto sourceFile = std::make_shared<SourceFile>(path);
+  auto root = makeNode(sourceFile, rootNode);
+  root->visit(&visitor);
+  auto optionState = OptionState(visitor.options);
+  ts_tree_delete(tree);
+  ts_parser_delete(parser);
+  return optionState;
+}
+
+OptionState parseOptions(std::filesystem::path root) {
+  auto modernOptionsFile = root / "meson.options";
   if (std::filesystem::exists(modernOptionsFile) &&
       std::filesystem::is_regular_file(modernOptionsFile)) {
-    TSParser *parser = ts_parser_new();
-    ts_parser_set_language(parser, tree_sitter_meson());
-    std::ifstream file(modernOptionsFile.c_str());
-    auto fileSize = std::filesystem::file_size(modernOptionsFile);
-    std::string fileContent;
-    fileContent.resize(fileSize, '\0');
-    file.read(fileContent.data(), fileSize);
-    TSTree *tree = ts_parser_parse_string(parser, NULL, fileContent.data(),
-                                          fileContent.length());
-    TSNode rootNode = ts_tree_root_node(tree);
-    auto sourceFile = std::make_shared<SourceFile>(modernOptionsFile);
-    auto root = makeNode(sourceFile, rootNode);
-    root->visit(&visitor);
-    optionState = OptionState(visitor.options);
-    ts_tree_delete(tree);
-    ts_parser_delete(parser);
-  } else {
-    auto legacyOptionsFile = this->root / "meson_options.txt";
-    if (std::filesystem::exists(legacyOptionsFile) &&
-        std::filesystem::is_regular_file(legacyOptionsFile)) {
-      TSParser *parser = ts_parser_new();
-      ts_parser_set_language(parser, tree_sitter_meson());
-      std::ifstream file(legacyOptionsFile.c_str());
-      auto fileSize = std::filesystem::file_size(legacyOptionsFile);
-      std::string fileContent;
-      fileContent.resize(fileSize, '\0');
-      file.read(fileContent.data(), fileSize);
-      TSTree *tree = ts_parser_parse_string(parser, NULL, fileContent.data(),
-                                            fileContent.length());
-      TSNode rootNode = ts_tree_root_node(tree);
-      auto sourceFile = std::make_shared<SourceFile>(legacyOptionsFile);
-      auto root = makeNode(sourceFile, rootNode);
-      root->visit(&visitor);
-      optionState = OptionState(visitor.options);
-      ts_tree_delete(tree);
-      ts_parser_delete(parser);
-    }
+    return parseFile(modernOptionsFile);
   }
+  auto legacyOptionsFile = root / "meson_options.txt";
+  if (std::filesystem::exists(legacyOptionsFile) &&
+      std::filesystem::is_regular_file(legacyOptionsFile)) {
+    return parseFile(legacyOptionsFile);
+  }
+  return {};
+}
+void MesonTree::fastParse(AnalysisOptions analysisOptions) {
+  // First fetch all the options
+  auto options = parseOptions(this->root);
   // Then parse the root meson.build file
 }
