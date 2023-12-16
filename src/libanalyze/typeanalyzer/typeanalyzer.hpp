500acb5e9206d6944b034c7343d37f67893d3fdd
JCWasmx86 <JCWasmx86@t-online.de>
2023-12-16 12:40:41 +0100

    Parse at least the root meson.build file

---
+++ src/libanalyze/typeanalyzer/typeanalyzer.hpp
@@ -0,0 +1,29 @@
+#pragma once
+
+#include "node.hpp"
+
+class TypeAnalyzer : public CodeVisitor {
+public:
+  void visitArgumentList(ArgumentList *node);
+  void visitArrayLiteral(ArrayLiteral *node);
+  void visitAssignmentStatement(AssignmentStatement *node);
+  void visitBinaryExpression(BinaryExpression *node);
+  void visitBooleanLiteral(BooleanLiteral *node);
+  void visitBuildDefinition(BuildDefinition *node);
+  void visitConditionalExpression(ConditionalExpression *node);
+  void visitDictionaryLiteral(DictionaryLiteral *node);
+  void visitFunctionExpression(FunctionExpression *node);
+  void visitIdExpression(IdExpression *node);
+  void visitIntegerLiteral(IntegerLiteral *node);
+  void visitIterationStatement(IterationStatement *node);
+  void visitKeyValueItem(KeyValueItem *node);
+  void visitKeywordItem(KeywordItem *node);
+  void visitMethodExpression(MethodExpression *node);
+  void visitSelectionStatement(SelectionStatement *node);
+  void visitStringLiteral(StringLiteral *node);
+  void visitSubscriptExpression(SubscriptExpression *node);
+  void visitUnaryExpression(UnaryExpression *node);
+  void visitErrorNode(ErrorNode *node);
+  void visitBreakNode(BreakNode *node);
+  void visitContinueNode(ContinueNode *node);
+};
