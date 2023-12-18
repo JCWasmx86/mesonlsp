4e048f16b9f965a823b82e76cfb8fc0512f3e902
JCWasmx86 <JCWasmx86@t-online.de>
2023-12-18 08:41:32 +0100

    Fix clang-tidy warning

--- src/libast/location.hpp
+++ src/libast/location.hpp
@@ -5,14 +5,17 @@
 #include <string>
 #include <tree_sitter/api.h>
 
+#define HALF_OF_U64 32U
+
 class Location {
 public:
   Location(uint32_t startLine, uint32_t endLine, uint32_t startColumn,
            uint32_t endColumn)
       : startLine(startLine), endLine(endLine), startColumn(startColumn),
         endColumn(endColumn),
-        columns((this->startColumn) | (((uint64_t)this->endColumn) << 32U)),
-        lines((this->startLine) | (((uint64_t)this->endLine) << 32U)) {}
+        columns((this->startColumn) |
+                (((uint64_t)this->endColumn) << HALF_OF_U64)),
+        lines((this->startLine) | (((uint64_t)this->endLine) << HALF_OF_U64)) {}
 
   Location()
       : startLine(0), endLine(0), startColumn(0), endColumn(0), columns(0),
@@ -23,8 +26,9 @@ public:
         endLine(ts_node_end_point(node).row),
         startColumn(ts_node_start_point(node).column / 2),
         endColumn(ts_node_end_point(node).column / 2),
-        columns((this->startColumn) | (((uint64_t)this->endColumn) << 32U)),
-        lines((this->startLine) | (((uint64_t)this->endLine) << 32U)) {}
+        columns((this->startColumn) |
+                (((uint64_t)this->endColumn) << HALF_OF_U64)),
+        lines((this->startLine) | (((uint64_t)this->endLine) << HALF_OF_U64)) {}
 
   const uint32_t startLine;
   const uint32_t endLine;
