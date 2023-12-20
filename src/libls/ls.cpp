45661f979ac3fcb9f5de276fde50127ad7af8312
JCWasmx86 <JCWasmx86@t-online.de>
2023-12-20 10:01:14 +0100

    Add first skeleton of libls

---
+++ src/libls/ls.cpp
@@ -0,0 +1,63 @@
+#include "ls.hpp"
+
+#include "jsonrpc.hpp"
+#include "log.hpp"
+#include "lsptypes.hpp"
+#include "nlohmann/json.hpp"
+
+#include <format>
+
+static Logger LOG("AbstractLanguageServer"); // NOLINT
+
+void AbstractLanguageServer::handleNotification(std::string method,
+                                                nlohmann::json params) {
+  LOG.info(std::format("Received notification {}", method));
+  if (method == "initialized") {
+    InitializedParams serializedParams;
+    this->onInitialized(serializedParams);
+    return;
+  }
+  if (method == "textDocument/didOpen") {
+    DidOpenTextDocumentParams serializedParams(params);
+    this->onDidOpenTextDocument(serializedParams);
+    return;
+  }
+  if (method == "textDocument/didClose") {
+    DidCloseTextDocumentParams serializedParams(params);
+    this->onDidCloseTextDocument(serializedParams);
+    return;
+  }
+  if (method == "textDocument/didChange") {
+    DidChangeTextDocumentParams serializedParams(params);
+    this->onDidChangeTextDocument(serializedParams);
+    return;
+  }
+  if (method == "textDocument/didSave") {
+    DidSaveTextDocumentParams serializedParams(params);
+    this->onDidSaveTextDocument(serializedParams);
+    return;
+  }
+  if (method == "exit") {
+    this->onExit();
+    return;
+  }
+  LOG.warn(std::format("Unknown notification: '{}'", method));
+}
+
+void AbstractLanguageServer::handleRequest(std::string method,
+                                           nlohmann::json callId,
+                                           nlohmann::json params) {
+  LOG.info(std::format("Received request: {}", method));
+  nlohmann::json ret;
+  if (method == "initialize") {
+    InitializeParams serializedParams(params);
+    auto results = this->initialize(serializedParams);
+    ret = results.toJson();
+  } else {
+    LOG.warn(std::format("Unknown request: '{}'", method));
+    this->server->returnError(jsonrpc::JsonrpcError::MethodNotFound,
+                              std::format("Unknown request: {}", method));
+    return;
+  }
+  this->server->reply(callId, ret);
+}
---
+++ src/libls/ls.hpp
@@ -0,0 +1,21 @@
+#include "jsonrpc.hpp"
+#include "lsptypes.hpp"
+
+#include <nlohmann/json.hpp>
+
+class AbstractLanguageServer : public jsonrpc::JsonRpcHandler {
+public:
+  void handleNotification(std::string method, nlohmann::json params) override;
+  void handleRequest(std::string method, nlohmann::json callId,
+                     nlohmann::json params) override;
+
+  virtual InitializeResult initialize(InitializeParams &params) = 0;
+  virtual void shutdown() = 0;
+
+  virtual void onInitialized(InitializedParams &params) = 0;
+  virtual void onExit() = 0;
+  virtual void onDidOpenTextDocument(DidOpenTextDocumentParams &params) = 0;
+  virtual void onDidChangeTextDocument(DidChangeTextDocumentParams &params) = 0;
+  virtual void onDidSaveTextDocument(DidSaveTextDocumentParams &params) = 0;
+  virtual void onDidCloseTextDocument(DidCloseTextDocumentParams &params) = 0;
+};
