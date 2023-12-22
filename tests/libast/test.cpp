d28d7cc4ffee9378409dfbc8a7de6a3e7f102a90
JCWasmx86 <JCWasmx86@t-online.de>
2023-12-22 18:52:03 +0100

    Add parser tests for nearly all nodes
    The other ones are pointless to test

--- tests/libast/test.cpp
+++ tests/libast/test.cpp
@@ -1,3 +1,4 @@
+#include "function.hpp"
 #include "node.hpp"
 #include "sourcefile.hpp"
 
@@ -235,6 +236,308 @@ TEST(TestAst, testBooleanLiteral) {
   rhs = dynamic_cast<BooleanLiteral *>(ass->rhs.get());
   ASSERT_NE(rhs, nullptr);
   ASSERT_FALSE(rhs->value);
+}
+
+TEST(TestAst, testConditionalExpression) {
+  auto node = parseToNode("true ? x : y\n");
+  auto *bd = dynamic_cast<BuildDefinition *>(node.get());
+  ASSERT_NE(bd, nullptr);
+  ASSERT_EQ(bd->stmts.size(), 1);
+  auto *ce = dynamic_cast<ConditionalExpression *>(bd->stmts[0].get());
+  ASSERT_NE(ce, nullptr);
+  ASSERT_FALSE(ce->ifTrue->equals(ce->ifFalse.get()));
+  ASSERT_FALSE(ce->ifTrue->equals(ce->condition.get()));
+  ASSERT_FALSE(ce->condition->equals(ce->ifFalse.get()));
+}
+
+TEST(TestAst, testDictionaryLiteral) {
+  auto node = parseToNode("{'foo': 1,\n# 'bar': 2,\n 'baz': 3\n}\n");
+  auto *bd = dynamic_cast<BuildDefinition *>(node.get());
+  ASSERT_NE(bd, nullptr);
+  ASSERT_EQ(bd->stmts.size(), 1);
+  auto *dictLit = dynamic_cast<DictionaryLiteral *>(bd->stmts[0].get());
+  ASSERT_NE(dictLit, nullptr);
+  ASSERT_EQ(dictLit->values.size(), 2);
+}
+
+TEST(TestAst, testIdExpression) {
+  auto node = parseToNode("x = 1\nxy = 2\nxyz = 3\n");
+  auto *bd = dynamic_cast<BuildDefinition *>(node.get());
+  ASSERT_NE(bd, nullptr);
+  ASSERT_EQ(bd->stmts.size(), 3);
+  ASSERT_EQ(
+      dynamic_cast<IdExpression *>(
+          dynamic_cast<AssignmentStatement *>(bd->stmts[0].get())->lhs.get())
+          ->id,
+      "x");
+  ASSERT_EQ(
+      dynamic_cast<IdExpression *>(
+          dynamic_cast<AssignmentStatement *>(bd->stmts[1].get())->lhs.get())
+          ->id,
+      "xy");
+  ASSERT_EQ(
+      dynamic_cast<IdExpression *>(
+          dynamic_cast<AssignmentStatement *>(bd->stmts[2].get())->lhs.get())
+          ->id,
+      "xyz");
+}
+
+TEST(TestAst, testFunctionExpression) {
+  auto node = parseToNode("project()\nfoo(1,2)\n");
+  auto *bd = dynamic_cast<BuildDefinition *>(node.get());
+  ASSERT_NE(bd, nullptr);
+  ASSERT_EQ(bd->stmts.size(), 2);
+  auto *fe = dynamic_cast<FunctionExpression *>(bd->stmts[0].get());
+  ASSERT_NE(fe, nullptr);
+  ASSERT_EQ(fe->functionName(), "project");
+  ASSERT_EQ(fe->args, nullptr);
+  fe = dynamic_cast<FunctionExpression *>(bd->stmts[1].get());
+  ASSERT_NE(fe, nullptr);
+  ASSERT_EQ(fe->functionName(), "foo");
+  ASSERT_NE(fe->args, nullptr);
+}
+
+TEST(TestAst, testIntegerLiteral) {
+  auto node = parseToNode(
+      "x = 123\nx = 0x11\nx = 0X11\nx = 0b11\nx = 0B11\nx = 0o755\nx = 0O755");
+  auto *bd = dynamic_cast<BuildDefinition *>(node.get());
+  ASSERT_NE(bd, nullptr);
+  ASSERT_EQ(bd->stmts.size(), 7);
+  std::vector<int> values = {123, 0x11, 0x11, 0b11, 0b11, 0755, 0755}; // NOLINT
+  for (size_t i = 0; i < bd->stmts.size(); i++) {
+    auto *ass = dynamic_cast<AssignmentStatement *>(bd->stmts[i].get());
+    ASSERT_NE(ass, nullptr);
+    auto *rhs = dynamic_cast<IntegerLiteral *>(ass->rhs.get());
+    ASSERT_NE(rhs, nullptr);
+    ASSERT_EQ(rhs->valueAsInt, values[i]);
+  }
+}
+
+TEST(TestAst, testIterationStatementEmpty) {
+  auto node = parseToNode("foreach x, y : []\n\nendforeach");
+  auto *bd = dynamic_cast<BuildDefinition *>(node.get());
+  ASSERT_NE(bd, nullptr);
+  ASSERT_EQ(bd->stmts.size(), 1);
+  auto *its = dynamic_cast<IterationStatement *>(bd->stmts[0].get());
+  ASSERT_NE(its, nullptr);
+  ASSERT_EQ(its->ids.size(), 2);
+  ASSERT_EQ(its->stmts.size(), 0);
+}
+
+TEST(TestAst, testIterationStatementComment) {
+  auto node = parseToNode("foreach x, #foo\n y : []\n# foo\n\nendforeach");
+  auto *bd = dynamic_cast<BuildDefinition *>(node.get());
+  ASSERT_NE(bd, nullptr);
+  ASSERT_EQ(bd->stmts.size(), 1);
+  auto *its = dynamic_cast<IterationStatement *>(bd->stmts[0].get());
+  ASSERT_NE(its, nullptr);
+  ASSERT_EQ(its->ids.size(), 2);
+  ASSERT_EQ(its->stmts.size(), 0);
+}
+
+TEST(TestAst, testIterationStatement) {
+  auto node =
+      parseToNode("foreach x, #foo\n y : []\n# foo\nbar()\n\nendforeach");
+  auto *bd = dynamic_cast<BuildDefinition *>(node.get());
+  ASSERT_NE(bd, nullptr);
+  ASSERT_EQ(bd->stmts.size(), 1);
+  auto *its = dynamic_cast<IterationStatement *>(bd->stmts[0].get());
+  ASSERT_NE(its, nullptr);
+  ASSERT_EQ(its->ids.size(), 2);
+  ASSERT_EQ(its->stmts.size(), 1);
+}
+
+TEST(TestAst, testNormalStringLiteral) {
+  auto node = parseToNode("x = 'foo'");
+  auto *bd = dynamic_cast<BuildDefinition *>(node.get());
+  ASSERT_NE(bd, nullptr);
+  ASSERT_EQ(bd->stmts.size(), 1);
+  auto *ass = dynamic_cast<AssignmentStatement *>(bd->stmts[0].get());
+  ASSERT_NE(ass, nullptr);
+  auto *sl = dynamic_cast<StringLiteral *>(ass->rhs.get());
+  ASSERT_FALSE(sl->isFormat);
+  ASSERT_EQ(sl->id, "foo");
+}
+
+TEST(TestAst, testMLStringLiteral) {
+  auto node = parseToNode("x = '''\nfoo\n'''");
+  auto *bd = dynamic_cast<BuildDefinition *>(node.get());
+  ASSERT_NE(bd, nullptr);
+  ASSERT_EQ(bd->stmts.size(), 1);
+  auto *ass = dynamic_cast<AssignmentStatement *>(bd->stmts[0].get());
+  ASSERT_NE(ass, nullptr);
+  auto *sl = dynamic_cast<StringLiteral *>(ass->rhs.get());
+  ASSERT_FALSE(sl->isFormat);
+  ASSERT_EQ(sl->id, "\nfoo\n");
+}
+
+TEST(TestAst, testNormalFStringLiteral) {
+  auto node = parseToNode("x = f'foo'");
+  auto *bd = dynamic_cast<BuildDefinition *>(node.get());
+  ASSERT_NE(bd, nullptr);
+  ASSERT_EQ(bd->stmts.size(), 1);
+  auto *ass = dynamic_cast<AssignmentStatement *>(bd->stmts[0].get());
+  ASSERT_NE(ass, nullptr);
+  auto *sl = dynamic_cast<StringLiteral *>(ass->rhs.get());
+  ASSERT_TRUE(sl->isFormat);
+  ASSERT_EQ(sl->id, "foo");
+}
+
+TEST(TestAst, testMLFStringLiteral) {
+  auto node = parseToNode("x = f'''\nfoo\n'''");
+  auto *bd = dynamic_cast<BuildDefinition *>(node.get());
+  ASSERT_NE(bd, nullptr);
+  ASSERT_EQ(bd->stmts.size(), 1);
+  auto *ass = dynamic_cast<AssignmentStatement *>(bd->stmts[0].get());
+  ASSERT_NE(ass, nullptr);
+  auto *sl = dynamic_cast<StringLiteral *>(ass->rhs.get());
+  ASSERT_TRUE(sl->isFormat);
+  ASSERT_EQ(sl->id, "\nfoo\n");
+}
+
+TEST(TestAst, testMethodExpression) {
+  auto node = parseToNode("x.foo()\nxyz.foo(1,2,3)\n");
+  auto *bd = dynamic_cast<BuildDefinition *>(node.get());
+  ASSERT_NE(bd, nullptr);
+  ASSERT_EQ(bd->stmts.size(), 2);
+  auto *me = dynamic_cast<MethodExpression *>(bd->stmts[0].get());
+  ASSERT_NE(me, nullptr);
+  ASSERT_EQ(me->args, nullptr);
+  ASSERT_FALSE(me->obj->equals(me->id.get()));
+  me = dynamic_cast<MethodExpression *>(bd->stmts[1].get());
+  ASSERT_NE(me, nullptr);
+  ASSERT_NE(me->args, nullptr);
+  ASSERT_FALSE(me->obj->equals(me->id.get()));
+  ASSERT_FALSE(me->id->equals(me->args.get()));
+  ASSERT_FALSE(me->obj->equals(me->args.get()));
+}
+
+TEST(TestAst, testSelectionStatementIf) {
+  auto node = parseToNode("if true\nfoo()\nendif");
+  auto *bd = dynamic_cast<BuildDefinition *>(node.get());
+  ASSERT_NE(bd, nullptr);
+  ASSERT_EQ(bd->stmts.size(), 1);
+  auto *sst = dynamic_cast<SelectionStatement *>(bd->stmts[0].get());
+  ASSERT_NE(sst, nullptr);
+  ASSERT_EQ(sst->conditions.size(), 1);
+  ASSERT_EQ(sst->blocks.size(), 1);
+  ASSERT_EQ(sst->blocks[0].size(), 1);
+}
+
+TEST(TestAst, testSelectionStatementEmpty) {
+  auto node = parseToNode("if true\nendif");
+  auto *bd = dynamic_cast<BuildDefinition *>(node.get());
+  ASSERT_NE(bd, nullptr);
+  ASSERT_EQ(bd->stmts.size(), 1);
+  auto *sst = dynamic_cast<SelectionStatement *>(bd->stmts[0].get());
+  ASSERT_NE(sst, nullptr);
+  ASSERT_EQ(sst->conditions.size(), 1);
+  ASSERT_EQ(sst->blocks.size(), 1);
+  ASSERT_EQ(sst->blocks[0].size(), 0);
+}
+
+TEST(TestAst, testSelectionStatementComment) {
+  auto node = parseToNode("if true\n#foo\nendif");
+  auto *bd = dynamic_cast<BuildDefinition *>(node.get());
+  ASSERT_NE(bd, nullptr);
+  ASSERT_EQ(bd->stmts.size(), 1);
+  auto *sst = dynamic_cast<SelectionStatement *>(bd->stmts[0].get());
+  ASSERT_NE(sst, nullptr);
+  ASSERT_EQ(sst->conditions.size(), 1);
+  ASSERT_EQ(sst->blocks.size(), 1);
+  ASSERT_EQ(sst->blocks[0].size(), 0);
+}
+
+TEST(TestAst, testSelectionStatementIf2) {
+  auto node = parseToNode("if true\n#foo\nfoo()\nendif");
+  auto *bd = dynamic_cast<BuildDefinition *>(node.get());
+  ASSERT_NE(bd, nullptr);
+  ASSERT_EQ(bd->stmts.size(), 1);
+  auto *sst = dynamic_cast<SelectionStatement *>(bd->stmts[0].get());
+  ASSERT_NE(sst, nullptr);
+  ASSERT_EQ(sst->conditions.size(), 1);
+  ASSERT_EQ(sst->blocks.size(), 1);
+  ASSERT_EQ(sst->blocks[0].size(), 1);
+}
+
+TEST(TestAst, testSelectionStatementIfElse) {
+  auto node = parseToNode("if true\nelse\nendif\n");
+  auto *bd = dynamic_cast<BuildDefinition *>(node.get());
+  ASSERT_NE(bd, nullptr);
+  ASSERT_EQ(bd->stmts.size(), 1);
+  auto *sst = dynamic_cast<SelectionStatement *>(bd->stmts[0].get());
+  ASSERT_NE(sst, nullptr);
+  ASSERT_EQ(sst->conditions.size(), 1);
+  ASSERT_EQ(sst->blocks.size(), 2);
+  ASSERT_EQ(sst->blocks[0].size(), 0);
+  ASSERT_EQ(sst->blocks[1].size(), 0);
+}
+
+TEST(TestAst, testSelectionStatementIfElse2) {
+  auto node = parseToNode("if true\nfoo()\nelse\n#Foo\nbar()\nendif\n");
+  auto *bd = dynamic_cast<BuildDefinition *>(node.get());
+  ASSERT_NE(bd, nullptr);
+  ASSERT_EQ(bd->stmts.size(), 1);
+  auto *sst = dynamic_cast<SelectionStatement *>(bd->stmts[0].get());
+  ASSERT_NE(sst, nullptr);
+  ASSERT_EQ(sst->conditions.size(), 1);
+  ASSERT_EQ(sst->blocks.size(), 2);
+  ASSERT_EQ(sst->blocks[0].size(), 1);
+  ASSERT_EQ(sst->blocks[1].size(), 1);
+}
+
+TEST(TestAst, testSelectionStatementIfIfElse) {
+  auto node = parseToNode("if true\nfoo()\nbar()\nelif 1 == "
+                          "1\nfoo()\nelse\n#Foo\nbar()\nx = 1\ny = 1\nendif\n");
+  auto *bd = dynamic_cast<BuildDefinition *>(node.get());
+  ASSERT_NE(bd, nullptr);
+  ASSERT_EQ(bd->stmts.size(), 1);
+  auto *sst = dynamic_cast<SelectionStatement *>(bd->stmts[0].get());
+  ASSERT_NE(sst, nullptr);
+  ASSERT_EQ(sst->conditions.size(), 2);
+  ASSERT_EQ(sst->blocks.size(), 3);
+  ASSERT_EQ(sst->blocks[0].size(), 2);
+  ASSERT_EQ(sst->blocks[1].size(), 1);
+  ASSERT_EQ(sst->blocks[2].size(), 3);
+}
+
+TEST(TestAst, testSubscriptExpression) {
+  auto node = parseToNode("foo[123]");
+  auto *bd = dynamic_cast<BuildDefinition *>(node.get());
+  ASSERT_NE(bd, nullptr);
+  ASSERT_EQ(bd->stmts.size(), 1);
+  auto *sse = dynamic_cast<SubscriptExpression *>(bd->stmts[0].get());
+  ASSERT_NE(sse, nullptr);
+  ASSERT_NE(dynamic_cast<IntegerLiteral *>(sse->inner.get()), nullptr);
+  ASSERT_NE(dynamic_cast<IdExpression *>(sse->outer.get()), nullptr);
+}
+
+TEST(TestAst, testUnaryOperator) {
+  auto node = parseToNode("x = not a\ny = !b\nz = -5\n");
+  auto *bd = dynamic_cast<BuildDefinition *>(node.get());
+  ASSERT_NE(bd, nullptr);
+  ASSERT_EQ(bd->stmts.size(), 3);
+  for (const auto &stmt : bd->stmts) {
+    auto *ass = dynamic_cast<AssignmentStatement *>(stmt.get());
+    ASSERT_NE(ass, nullptr);
+    auto *unaryExpr = dynamic_cast<UnaryExpression *>(ass->rhs.get());
+    ASSERT_NE(unaryExpr, nullptr);
+  }
+  ASSERT_EQ(
+      dynamic_cast<UnaryExpression *>(
+          dynamic_cast<AssignmentStatement *>(bd->stmts[0].get())->rhs.get())
+          ->op,
+      UnaryOperator::Not);
+  ASSERT_EQ(
+      dynamic_cast<UnaryExpression *>(
+          dynamic_cast<AssignmentStatement *>(bd->stmts[1].get())->rhs.get())
+          ->op,
+      UnaryOperator::ExclamationMark);
+  ASSERT_EQ(
+      dynamic_cast<UnaryExpression *>(
+          dynamic_cast<AssignmentStatement *>(bd->stmts[2].get())->rhs.get())
+          ->op,
+      UnaryOperator::UnaryMinus);
 }
 
 int main(int argc, char **argv) {
