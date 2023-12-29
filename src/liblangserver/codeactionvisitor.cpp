dcc156e832503739c08509f12d90ebb14dd833ad
JCWasmx86 <JCWasmx86@t-online.de>
2023-12-29 11:50:09 +0100

    Add code action to generate declare_dependency

--- src/liblangserver/codeactionvisitor.cpp
+++ src/liblangserver/codeactionvisitor.cpp
@@ -26,6 +26,7 @@ bool CodeActionVisitor::inRange(const Node *node, bool add) {
     }
     makeIntegerToBaseAction(node);
     makeCopyFileAction(node);
+    makeDeclareDependencyAction(node);
     return true;
   }
   return false;
@@ -279,4 +280,92 @@ bool CodeActionVisitor::expectedArgsForCopyFile(const ArgumentList *al) {
     return false;
   }
   return boolLit->value;
+}
+
+void CodeActionVisitor::makeDeclareDependencyAction(const Node *node) {
+  const auto *fExpr = dynamic_cast<const FunctionExpression *>(node);
+  if (!fExpr) {
+    return;
+  }
+  auto func = fExpr->function;
+  if (!func || !CodeActionVisitor::createsLibrary(func)) {
+    return;
+  }
+  auto libnameOpt = CodeActionVisitor::extractVariablename(fExpr);
+  if (!libnameOpt.has_value()) {
+    return;
+  }
+  auto libname = libnameOpt.value();
+  const auto *args = dynamic_cast<const ArgumentList *>(fExpr->args.get());
+  if (!args) {
+    return;
+  }
+  std::string dependencyName;
+  if (libname.size() >= 4 &&
+      libname.compare(libname.size() - 4, 4, "_lib") == 0) {
+    dependencyName = libname;
+    dependencyName.replace(dependencyName.size() - 4, 4, "_dep");
+  } else if (libname.size() >= 4 && libname.compare(0, 4, "lib_") == 0) {
+    dependencyName = libname;
+    dependencyName.replace(0, 4, "dep_");
+  } else {
+    dependencyName = "dep_" + libname;
+  }
+  if (this->tree->scope.variables.contains(dependencyName)) {
+    return;
+  }
+  auto depname = "dep_" + libname;
+  if (this->tree->scope.variables.contains(depname)) {
+    return;
+  }
+  depname = libname;
+  depname = replace(depname, "lib_", "dep_");
+  auto tmpDepname = libname;
+  replace(tmpDepname, "lib_", "dep_");
+  if (this->tree->scope.variables.contains(tmpDepname) && depname != libname) {
+    return;
+  }
+  depname = libname;
+  depname = replace(depname, "_lib", "_dep");
+  if (this->tree->scope.variables.contains(depname) && depname != libname) {
+    return;
+  }
+  auto nextLine = fExpr->parent->location->endLine + 1;
+  auto str = std::format("{} = declare_dependency(\n", dependencyName);
+  for (const auto &kwargName : std::vector<std::string>{
+           "d_import_dirs", "d_module_versions", "dependencies",
+           "include_directories", "link_args", "link_whole", "link_with",
+           "objects", "version"}) {
+    auto kwarg = args->getKwarg(kwargName);
+    if (!kwarg.has_value()) {
+      if (kwargName == "link_with") {
+        str += std::format("link_with: [{}],\n", libname);
+      }
+      continue;
+    }
+    if (kwargName == "link_with") {
+      const auto *idExpr = dynamic_cast<const IdExpression *>(kwarg->get());
+      if (idExpr) {
+        str += std::format("link_with: [{}, {}],\n", idExpr->id, libname);
+      }
+      const auto *arrLi = dynamic_cast<const ArrayLiteral *>(kwarg->get());
+      if (arrLi) {
+        auto str = kwarg.value()
+                       ->file->extractNodeValue(kwarg.value()->location)
+                       .substr(1);
+        str += std::format("link_with: [{}, {}, \n", libname, str);
+      }
+      continue;
+    }
+    str += std::format(
+        "{}: {},\n", kwargName,
+        kwarg.value()->file->extractNodeValue(kwarg.value()->location));
+  }
+  str += ")\n";
+  auto range = LSPRange(LSPPosition(nextLine, 0), LSPPosition(nextLine, 0));
+  auto textEdit = TextEdit(range, str);
+  WorkspaceEdit edit;
+  edit.changes[this->uri] = {textEdit};
+  this->actions.emplace_back(
+      std::format("Declare dependency {} for library", dependencyName), edit);
 }
--- src/liblangserver/codeactionvisitor.hpp
+++ src/liblangserver/codeactionvisitor.hpp
@@ -1,9 +1,12 @@
 #pragma once
 
+#include "function.hpp"
 #include "lsptypes.hpp"
 #include "mesontree.hpp"
 #include "node.hpp"
 
+#include <memory>
+#include <optional>
 #include <string>
 #include <utility>
 #include <vector>
@@ -46,7 +49,31 @@ private:
   bool inRange(const Node *node, bool add = true);
   void makeIntegerToBaseAction(const Node *node);
   void makeCopyFileAction(const Node *node);
+  void makeDeclareDependencyAction(const Node *node);
   void makeActionForBase(const IntegerLiteral *il, const std::string &title,
                          const std::string &prefix, const std::string &val);
   bool expectedArgsForCopyFile(const ArgumentList *al);
+
+  static bool createsLibrary(const std::shared_ptr<Function> &func) {
+    auto name = func->id();
+    return name == "static_library" || name == "shared_library" ||
+           name == "library";
+  }
+
+  static std::optional<std::string>
+  extractVariablename(const FunctionExpression *fExpr) {
+    const auto *parent = fExpr->parent;
+    if (!parent) {
+      return std::nullopt;
+    }
+    const auto *ass = dynamic_cast<const AssignmentStatement *>(parent);
+    if (!ass) {
+      return std::nullopt;
+    }
+    const auto *idExpr = dynamic_cast<const IdExpression *>(ass->lhs.get());
+    if (!idExpr) {
+      return std::nullopt;
+    }
+    return idExpr->id;
+  }
 };
