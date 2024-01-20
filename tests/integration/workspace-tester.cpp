6e70dd8d786e663e3d9e469db1c9979f875126b0
JCWasmx86 <JCWasmx86@t-online.de>
2024-01-20 15:41:38 +0100

    Add very basic tests

---
+++ tests/integration/workspace-tester.cpp
@@ -0,0 +1,69 @@
+#include "langserveroptions.hpp"
+#include "langserverutils.hpp"
+#include "log.hpp"
+#include "lsptypes.hpp"
+#include "mesonmetadata.hpp"
+#include "nlohmann/json.hpp"
+#include "typenamespace.hpp"
+#include "workspace.hpp"
+
+#include <cassert>
+#include <filesystem>
+#include <format>
+#include <string>
+
+int main(int argc, char **argv) {
+  Logger const logger("workspace-tester");
+  std::filesystem::path const toParse = argv[1];
+  const auto url = pathToUrl(toParse.parent_path());
+  LanguageServerOptions options;
+  nlohmann::json json;
+  json["uri"] = url;
+  json["name"] = "root";
+  TypeNamespace ns;
+  WorkspaceFolder wspf{json};
+  Workspace workspace{wspf, options};
+  const auto diags = workspace.parse(ns);
+  assert(diags.size() == 1);
+  const auto &firstDiags = diags.begin()->second;
+  assert(firstDiags.size() == 1);
+  const auto &diag = firstDiags[0];
+  assert(diag.severity == DiagnosticSeverity::LSPWarning);
+  assert(diag.message ==
+         "Meson version 0.21.0 is requested, but meson.project_build_root() is "
+         "only available since 0.56.0");
+  auto gotoDefinition = workspace.jumpTo(diags.begin()->first, {9, 8});
+  assert(gotoDefinition.size() == 1);
+  assert(gotoDefinition[0].range.start.line == 4);
+  assert(gotoDefinition[0].range.start.character == 0);
+  gotoDefinition = workspace.jumpTo(diags.begin()->first, {10, 8});
+  assert(gotoDefinition.size() == 1);
+  assert(gotoDefinition[0].range.start.line == 0);
+  assert(gotoDefinition[0].range.start.character == 7);
+  gotoDefinition = workspace.jumpTo(diags.begin()->first, {0, 2});
+  assert(gotoDefinition.empty());
+  assert(workspace.owns(diags.begin()->first));
+  gotoDefinition = workspace.jumpTo(diags.begin()->first, {11, 8});
+  assert(gotoDefinition.size() == 1);
+  assert(gotoDefinition[0].uri.ends_with("subdir/meson.build"));
+  assert(gotoDefinition[0].range.start.line == 0);
+  assert(gotoDefinition[0].range.start.character == 0);
+  nlohmann::json renameJsonParams;
+  renameJsonParams["textDocument"] = {{"uri", diags.begin()->first}};
+  renameJsonParams["newName"] = "foo123";
+  renameJsonParams["position"] = {{"line", 0}, {"character", 0}};
+  auto renameEdit =
+      workspace.rename(diags.begin()->first, RenameParams(renameJsonParams));
+  assert(!renameEdit.has_value());
+  renameJsonParams["textDocument"] = {{"uri", diags.begin()->first}};
+  renameJsonParams["newName"] = "foo123";
+  renameJsonParams["position"] = {{"line", 9}, {"character", 8}};
+  renameEdit =
+      workspace.rename(diags.begin()->first, RenameParams(renameJsonParams));
+  assert(renameEdit.has_value());
+  // Root + Subdir file
+  assert(renameEdit->changes.size() == 2);
+  auto &renameChanges = renameEdit->changes.begin()->second;
+  assert(renameChanges.size() == 3);
+  logger.info("Success");
+}
