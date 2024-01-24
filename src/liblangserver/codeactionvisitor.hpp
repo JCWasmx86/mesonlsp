cd45a6adce6f699014e7904d212d5a128b0afed7
JCWasmx86 <JCWasmx86@t-online.de>
2024-01-24 10:06:51 +0100

    Use std::function

--- src/liblangserver/codeactionvisitor.hpp
+++ src/liblangserver/codeactionvisitor.hpp
@@ -69,9 +69,9 @@ private:
   void postSorting(const std::string &msg, size_t omitCount,
                    const ArgumentList *args,
                    const std::vector<const Node *> &sortedNodes);
-  void makeSortFilenamesAction(const Node *node,
-                               bool (*sortFn)(const Node *, const Node *),
-                               const std::string &msg);
+  void makeSortFilenamesAction(
+      const Node *node, std::function<bool(const Node *, const Node *)> sortFn,
+      const std::string &msg);
 
   static bool createsLibrary(const std::shared_ptr<Function> &func) {
     auto name = func->id();
