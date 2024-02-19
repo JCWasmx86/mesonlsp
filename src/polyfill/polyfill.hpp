0ff3ac48277e6311f7e77915e5a524e49249581f
JCWasmx86 <JCWasmx86@t-online.de>
2024-02-19 15:05:53 +0100

    Minor fixes

--- src/polyfill/polyfill.hpp
+++ src/polyfill/polyfill.hpp
@@ -1,21 +1,25 @@
 #pragma once
 
-#ifndef __cpp_lib_format
-  #include <fmt/core.h>
-  namespace std {
-    using fmt::format;
-  }
-  template <typename Clock, typename Duration>
-struct fmt::formatter<std::chrono::time_point<Clock, Duration>> {
-    // Presentation format: 'f' for full format
-    constexpr auto parse(format_parse_context& ctx) { return ctx.begin(); }
+#ifdef __APPLE__
+#include <chrono>
+#include <fmt/core.h>
 
-    // Format the time point
-    template <typename FormatContext>
-    auto format(const std::chrono::time_point<Clock, Duration>& tp, FormatContext& ctx) {
-        return fmt::format_to(ctx.out(), "{}", tp.time_since_epoch().count());
-    }
+namespace std {
+using fmt::format;
+}
+
+template <typename Clock, typename Duration>
+struct fmt::formatter<std::chrono::time_point<Clock, Duration>> {
+  // Presentation format: 'f' for full format
+  constexpr auto parse(format_parse_context &ctx) { return ctx.begin(); }
+
+  // Format the time point
+  template <typename FormatContext>
+  auto format(const std::chrono::time_point<Clock, Duration> &tp,
+              FormatContext &ctx) {
+    return fmt::format_to(ctx.out(), "{}", tp.time_since_epoch().count());
+  }
 };
 #else
-  #include <format>
+#include <format>
 #endif
