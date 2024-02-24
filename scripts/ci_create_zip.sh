5f50b78deb71849b962867db4cf138beeb3dadf7
JCWasmx86 <JCWasmx86@t-online.de>
2024-02-24 18:37:44 +0100

    Test out more stacktraces

--- scripts/ci_create_zip.sh
+++ scripts/ci_create_zip.sh
@@ -1,11 +1,13 @@
 #!/usr/bin/env bash
 meson setup _release --buildtype release -Db_lto=true
 ninja -C _release || exit 1
+./_release/tests/libcxathrow/cxathrowtest
 cp _release/src/mesonlsp mesonlsp
 rm -rf _release
 meson setup _build --buildtype debug
 ninja -C _build || exit 1
 ninja -C _build test || exit 1
+./_build/tests/libcxathrow/cxathrowtest
 cp _build/src/mesonlsp mesonlsp.debug
 zip -9 "$1".zip mesonlsp.debug mesonlsp
 sudo cp "$1".zip / || true
