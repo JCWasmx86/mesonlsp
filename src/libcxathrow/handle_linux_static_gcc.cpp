22ac86ace124498c597932a7ab785df98671b6e2
JCWasmx86 <JCWasmx86@t-online.de>
2024-02-24 18:08:34 +0100

    Actually log backtrace

--- src/libcxathrow/handle_linux_static_gcc.cpp
+++ src/libcxathrow/handle_linux_static_gcc.cpp
@@ -8,5 +8,6 @@ void __wrap___cxa_throw /*NOLINT*/
     (void * /*NOLINT*/ thrown_exception, void *pvtinfo, void (*dest)(void *)) {
   const auto *typeinfo = (const std::type_info *)pvtinfo;
   logExceptionType(typeinfo);
+  doBacktrace();
   __real___cxa_throw(thrown_exception, pvtinfo, dest);
 }
