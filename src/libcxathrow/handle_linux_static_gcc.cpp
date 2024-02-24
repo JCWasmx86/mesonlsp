1253bdb9c0a9ec9c8aac007fbb9813bcc0017929
JCWasmx86 <JCWasmx86@t-online.de>
2024-02-24 18:55:02 +0100

    Fix static builds

--- src/libcxathrow/handle_linux_static_gcc.cpp
+++ src/libcxathrow/handle_linux_static_gcc.cpp
@@ -4,7 +4,7 @@
 
 extern "C" void /*NOLINT*/ __real___cxa_throw(void *, void *, void (*)(void *));
 
-void __wrap___cxa_throw /*NOLINT*/
+extern "C" void __wrap___cxa_throw /*NOLINT*/
     (void * /*NOLINT*/ thrown_exception, void *pvtinfo, void (*dest)(void *)) {
   const auto *typeinfo = (const std::type_info *)pvtinfo;
   logExceptionType(typeinfo);
