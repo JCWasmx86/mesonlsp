fad6b0a2c8c19e78d99f5865a47fb73767a97482
JCWasmx86 <JCWasmx86@t-online.de>
2024-02-25 06:26:32 +0100

    Demangle symbol names with execinfo.h backtraces

--- src/libcxathrow/backtrace_execinfo.cpp
+++ src/libcxathrow/backtrace_execinfo.cpp
@@ -2,7 +2,11 @@
 #include "polyfill.hpp"
 #include "shared.hpp"
 
+#include <cstdlib>
+#include <cxxabi.h>
+#include <dlfcn.h>
 #include <execinfo.h>
+#include <format>
 
 const static Logger LOG("cxathrow"); // NOLINT
 constexpr auto BACKTRACE_LENGTH = 100;
@@ -10,9 +14,27 @@ constexpr auto BACKTRACE_LENGTH = 100;
 void doBacktrace() {
   void *backtraces[BACKTRACE_LENGTH];
   auto btSize = backtrace(backtraces, BACKTRACE_LENGTH);
-  auto *btSyms = backtrace_symbols(backtraces, btSize);
   for (auto i = 0; i < btSize; i++) {
-    LOG.debug(std::format("#{}: {}", i, btSyms[i]));
+    Dl_info info;
+    if (dladdr(backtraces[i], &info) != 0) {
+      if (info.dli_sname == nullptr) {
+        info.dli_sname = "???";
+      }
+      if (info.dli_saddr == nullptr) {
+        info.dli_saddr = backtraces[i];
+      }
+      auto *demangled =
+          abi::__cxa_demangle(info.dli_sname, nullptr, nullptr, nullptr);
+      auto offset =
+          (unsigned char *)backtraces[i] - (unsigned char *)info.dli_saddr;
+      const auto *symName = demangled ? demangled : info.dli_sname;
+      LOG.debug(std::format("#{}: {}({}+{:#x}) [{}]", i, info.dli_fname,
+                            symName, offset, backtraces[i]));
+      if (demangled) {
+        free(demangled);
+      }
+    } else {
+      LOG.debug(std::format("#{}: {}", i, backtraces[i]));
+    }
   }
-  free(btSyms);
 }
