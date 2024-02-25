c2bd39e979841db8db4e4a3d08be51a5bdcd282f
JCWasmx86 <JCWasmx86@t-online.de>
2024-02-25 07:42:10 +0100

    Use libunwind demangling

--- src/libcxathrow/backtrace_libunwind.cpp
+++ src/libcxathrow/backtrace_libunwind.cpp
@@ -1,9 +1,16 @@
+#include <cstdlib>
+#include <format>
 #define UNW_LOCAL_ONLY
+#include "log.hpp"
 #include "polyfill.hpp"
 #include "shared.hpp"
 
 #include <cstdint>
+#include <cxxabi.h>
 #include <libunwind.h>
+
+constexpr auto SYMBOL_LENGTH = 2048;
+const static Logger LOG("cxathrow"); // NOLINT
 
 void doBacktrace() {
   unw_cursor_t cursor;
@@ -15,8 +22,21 @@ void doBacktrace() {
   uint32_t idx = 0;
   while (unw_step(&cursor) > 0) {
     unw_word_t /*NOLINT*/ pc;
+    unw_word_t offset;
     unw_get_reg(&cursor, UNW_REG_IP, &pc);
-    printAddr(idx, (void *)pc);
+    std::array<char, SYMBOL_LENGTH> symbolName;
+    if (!unw_get_proc_name(&cursor, symbolName, sizeof(symbolName), &offset)) {
+      auto *demangled =
+          abi::__cxa_demangle(symbolName.data(), nullptr, nullptr, nullptr);
+      const auto *symName = demangled ? demangled : symbolName.data();
+      LOG.debug(
+          std::format("#{}: {}+{:#x} [{}]", idx, symName, offset, (void *)pc));
+      if (demangled) {
+        free(demangled);
+      }
+    } else {
+      LOG.debug(std::format("#{}: {}", idx, (void *)pc));
+    }
     idx++;
   }
 }
