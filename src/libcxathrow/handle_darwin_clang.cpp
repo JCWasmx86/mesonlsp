1b3304f1555c3c776bd56a69578c8f65e8606c57
JCWasmx86 <JCWasmx86@t-online.de>
2024-02-25 08:39:01 +0100

    Add missed file

---
+++ src/libcxathrow/handle_darwin_clang.cpp
@@ -0,0 +1,16 @@
+#include "shared.hpp"
+
+#include <cxxabi.h>
+#include <dlfcn.h>
+CxaThrowType origCxaThrow = nullptr;
+
+extern "C" void __cxa_throw /*NOLINT*/ (void *thrown_exception,
+                                        std::type_info *typeinfo,
+                                        void (*dest)(void *)) {
+  if (origCxaThrow == nullptr) {
+    origCxaThrow = (CxaThrowType)dlsym(RTLD_NEXT, "__cxa_throw");
+  }
+  logExceptionType(thrown_exception, typeinfo);
+  doBacktrace();
+  origCxaThrow(thrown_exception, typeinfo, dest);
+}
