cb9758ea12b81b3edabf9eb43b739286b50d5d0c
JCWasmx86 <JCWasmx86@t-online.de>
2024-03-05 10:36:55 +0100

    Adjust LSP test script

--- LSPTests/test.py
+++ LSPTests/test.py
@@ -1,20 +1,17 @@
 #!/usr/bin/env python3
-import logging
-import sys
 import asyncio
+import logging
+import pathlib
+import sys
+
 from lsprotocol import types
-from pygls import uris
 from pygls.lsp.client import BaseLanguageClient
-from pygls.protocol import LanguageServerProtocol
 from pygls.protocol import default_converter
+
 
 class LanguageClient(BaseLanguageClient):
     def __init__(self):
         super().__init__("test-client", "1.0.0", converter_factory=default_converter)
-        self.diagnostics: Dict[str, List[types.Diagnostic]] = {}
-        self.messages: List[types.ShowMessageParams] = []
-        self.log_messages: List[types.LogMessageParams] = []
-
 
 async def main():
     client = LanguageClient()
@@ -27,26 +24,47 @@ async def main():
         print(log_level, params.message)
 
     @client.feature(types.WINDOW_WORK_DONE_PROGRESS_CREATE)
-    def create_work_done_progress(client: LanguageClient, params: types.WorkDoneProgressParams):
+    def create_work_done_progress(
+        client: LanguageClient, params: types.WorkDoneProgressParams
+    ):
         print(params)
+
     @client.feature(types.PROGRESS)
     def progress(client: LanguageClient, params: types.ProgressParams):
         print(params)
-    fixture_dir = sys.argv[2]
+
+    @client.feature(types.TEXT_DOCUMENT_PUBLISH_DIAGNOSTICS)
+    def diagnostics(client: LanguageClient, params: types.PublishDiagnosticsParams):
+        print("DIAGS", params)
+
+    fixture_dir = str(pathlib.Path(sys.argv[2]).parent.resolve())
     await client.start_io(sys.argv[1], "--lsp")
-    response = await client.initialize_async(
+    await client.initialize_async(
         types.InitializeParams(
             capabilities=types.ClientCapabilities(),
-            root_path=fixture_dir,
+            workspace_folders=[types.WorkspaceFolder("file://" + fixture_dir, "root")],
         )
     )
-    print(response)
+    client.initialized(types.InitializedParams())
+    main_meson = "file://" + fixture_dir + "/meson.build"
+    await asyncio.sleep(1)
+    response = await client.text_document_document_symbol_async(
+        types.DocumentSymbolParams(types.TextDocumentIdentifier(main_meson))
+    )
+    assert len(response) == 1
+    response = await client.text_document_inlay_hint_async(
+        types.InlayHintParams(
+            types.TextDocumentIdentifier(main_meson),
+            types.Range(types.Position(0, 0), types.Position(200, 200)),
+        )
+    )
+    assert len(response) == 1
+    await client.shutdown_async(None)
+    client.exit(None)
+
 
 if __name__ == "__main__":
     logging.basicConfig()
     logger = logging.getLogger(__name__)
     logger.setLevel(logging.DEBUG)
-    loop = asyncio.get_event_loop()
-    asyncio.ensure_future(main())
-    loop.run_forever()
-    loop.close()
+    asyncio.run(main())
