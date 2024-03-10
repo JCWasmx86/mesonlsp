bf69af331a4f8d54b698cf3b7dd48dc876de3a8f
JCWasmx86 <JCWasmx86@t-online.de>
2024-03-10 18:49:31 +0100

    Fixup to previous commit

--- tests/libutils/processtest.cpp
+++ tests/libutils/processtest.cpp
@@ -12,7 +12,7 @@ int main(int /*argc*/, char ** /*argv*/) {
   assert(sout.value().contains("curl"));
   auto sout2 = captureProcessOutput("does-not-exist-executable", {});
   assert(!sout2.has_value());
-  auto sout3 = captureProcessOutput("pkg-config", {"--list-package-names"});
+  auto sout3 = captureProcessOutput("pkg-config", {"--list-all"});
   assert(sout3.has_value());
   return 0;
 }
