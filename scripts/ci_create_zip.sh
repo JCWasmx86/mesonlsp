30c48d8e1d0742bb6557473e209c44624d9b04fb
JCWasmx86 <JCWasmx86@t-online.de>
2024-04-14 14:59:53 +0200

    Fix

--- scripts/ci_create_zip.sh
+++ scripts/ci_create_zip.sh
@@ -1,12 +1,15 @@
 #!/usr/bin/env bash
 meson setup _release --buildtype release -Db_lto=true
-ninja -C _release "$2" || exit 1
+# shellcheck disable=SC2086
+ninja -C _release $2 || exit 1
 ./_release/tests/libcxathrow/cxathrowtest
 cp _release/src/mesonlsp mesonlsp
 rm -rf _release
 meson setup _build --buildtype debug
-ninja -C _build "$2" || exit 1
-ninja -C _build test "$2" || exit 1
+# shellcheck disable=SC2086
+ninja -C _build $2 || exit 1
+# shellcheck disable=SC2086
+inja -C _build test $2 || exit 1
 ./_build/tests/libcxathrow/cxathrowtest
 cp _build/src/mesonlsp mesonlsp.debug
 zip -9 "$1".zip mesonlsp.debug mesonlsp
